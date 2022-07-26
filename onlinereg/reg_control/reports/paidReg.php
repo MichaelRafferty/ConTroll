c50cd01bb95512cd7830752b88f97950419e9dde
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-07-26 14:35:07 -0400

    add AgeList and safequery

--- onlinereg/reg_control/reports/paidReg.php
+++ onlinereg/reg_control/reports/paidReg.php
@@ -27,32 +27,34 @@ $conid=$con['id'];
 header('Content-Type: application/csv');
 header('Content-Disposition: attachment; filename="members.csv"');
 
-$query = "SELECT P.first_name, P.last_name, P.email_addr"
-        . ", M.label, R.paid, R.create_trans, T.paid"
-        . ", Y.type, Y.description, Y.amount"
-    . " FROM memList as M"
-        . " JOIN reg as R on R.memId=M.id"
-        . " JOIN transaction as T on T.id=R.create_trans"
-        . " JOIN perinfo as P on P.id=R.perid"
-        . " JOIN payments as Y ON Y.transid=T.id"
-    . " WHERE M.memCategory in ('standard', 'yearahead') and M.conid=54"
-    . " ORDER BY create_trans;";
+// this query had a hard code of conid=54 in it, changed that to $conid
+$query = <<<EOS
+SELECT P.first_name, P.last_name, P.email_addr, A.label, R.paid, R.create_trans, T.paid, Y.type, Y.description, Y.amount
+FROM memList M
+JOIN reg R ON (R.memId=M.id)
+JOIN ageList A ON (M.memAge = A.ageType and M.conid = A.conid)
+JOIN transaction T ON (T.id=R.create_trans)
+JOIN perinfo P ON (P.id=R.perid)
+JOIN payments AS Y ON (Y.transid=T.id)
+WHERE M.memCategory in ('standard', 'yearahead') and M.conid=?
+ORDER BY create_trans;
+EOS;
 
-$query = "SELECT T.id, Y.type, Y.description"
-        . ", COUNT(DISTINCT R.perid) as people, COUNT(DISTINCT P.email_addr) as emails"
-        . ", T.create_date, SUM(R.paid) as reg_paid, T.paid, Y.amount"
-    . " FROM memList as M"
-        . " JOIN reg as R on R.memId=M.id"
-        . " JOIN perinfo as P on P.id=R.perid"
-        . " JOIN transaction as T on T.id=R.create_trans"
-        . " JOIN payments as Y on Y.transid=T.id"
-    . " WHERE M.conid=54 and M.memCategory in ('standard', 'yearahead')"
-    . " GROUP BY T.id ORDER BY emails;";
+// what is this query that was left in the file
+//$query = "SELECT T.id, Y.type, Y.description"
+//        . ", COUNT(DISTINCT R.perid) as people, COUNT(DISTINCT P.email_addr) as emails"
+//        . ", T.create_date, SUM(R.paid) as reg_paid, T.paid, Y.amount"
+//    . " FROM memList as M"
+//        . " JOIN reg as R on R.memId=M.id"
+//        . " JOIN perinfo as P on P.id=R.perid"
+//        . " JOIN transaction as T on T.id=R.create_trans"
+//        . " JOIN payments as Y on Y.transid=T.id"
+//    . " WHERE M.conid=54 and M.memCategory in ('standard', 'yearahead')"
+//    . " GROUP BY T.id ORDER BY emails;";
 
-echo "First Name, Last Name, Email, Type, Price, Transaction, Total, Method, Description, Paid "
-    . "\n";
+echo "First Name, Last Name, Email, Type, Price, Transaction, Total, Method, Description, Paid\n";
 
-$reportR = dbQuery($query);
+$reportR = dbSafeQuery($query, 'i', array($conid));
 while($reportL = fetch_safe_array($reportR)) {
     for($i = 0 ; $i < count($reportL); $i++) {
         printf("\"%s\",", $reportL[$i]);
