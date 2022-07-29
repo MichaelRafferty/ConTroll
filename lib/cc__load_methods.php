5521f51ade961071b66cc3aa0efa9ae926c06797
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-07-29 15:31:06 -0400

    move credit card functions to top level lib

--- lib/cc__load_methods.php
+++ lib/cc__load_methods.php
@@ -7,21 +7,21 @@ function load_cc_procs() {
 
     switch ($cc['type']) {
         case 'convergepay':
-            require_once("lib/convergepay.php");
+            require_once("cc_convergepay.php");
             break;
         case 'square':
-            require_once("lib/square.php");
+            require_once("cc_square.php");
             break;
         case 'test':
             if(($cc['env'] != 'sandbox') || $reg['test'] != 1) {
                 ajaxSuccess(array('status'=>'error','data'=>'Something thinks this is a real charge method'));
                 exit();
             }
-            require_once("lib/test.php");
+            require_once("cc_test.php");
             break;
         case 'bypass':
             if (str_contains($con['server'], '//127.0.0.1') || str_contains($con['server'], '//192.168.149.128') || $reg['test'] == 1) {
-                require_once("lib/bypass.php");
+                require_once("cc_bypass.php");
                 break;
             } else {
                 echo "Bypass is not a valid credit card processor for this configuration\n";
