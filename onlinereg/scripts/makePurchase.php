577eee4ebedd87a326d43c8b4c6296baa47a2961
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-08-01 16:05:47 -0400

    Move ajax_functions to /lib

--- onlinereg/scripts/makePurchase.php
+++ onlinereg/scripts/makePurchase.php
@@ -1,7 +1,6 @@
 <?php
-
-require_once "../lib/ajax_functions.php";
 require_once(__DIR__ . "/../../lib/db_functions.php");
+require_once(__DIR__ . "/../../lib/ajax_functions.php");
 require_once "../lib/log.php";
 require_once("../../lib/cc__load_methods.php");
 require_once("../../lib/email__load_methods.php");
