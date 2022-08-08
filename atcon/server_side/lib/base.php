920608334fd1c81f6e83ff525c6e68f0fb93e2ab
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-08-08 19:05:12 -0400

    debugging

--- atcon/server_side/lib/base.php
+++ atcon/server_side/lib/base.php
@@ -3,9 +3,10 @@
 global $db_ini;
 if (!$db_ini) {    
     $db_ini = parse_ini_file(__DIR__ . "/../../../config/reg_conf.ini", true);
+}
 
-f ($db_ini['reg']['https'] <> 0) {
-    if(!isset($_SERVER['HTTPS']) or $_SERVER["HTTPS"] != "on") {
+if ($db_ini['reg']['https'] <> 0) {
+    if (!isset($_SERVER['HTTPS']) OR $_SERVER["HTTPS"] != "on") {
         header("HTTP/1.1 301 Moved Permanently");
         header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
         exit();
@@ -28,7 +29,7 @@ function callOut($url, $data) {
    curl_close($ch);
 }
 
-function nullifnotsetempty($item)
+function nullifnotsetempty($item) {
     if (isset($item)) {
         if ($item != '')
             return $item;
