4b5ee38c350fa7ca4d25063933dd20eaf864aaa0
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-10-02 13:31:10 -0400

    move log.php to main lib directory

--- onlinereg/scripts/makePurchase.php
+++ onlinereg/scripts/makePurchase.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . "/../../lib/db_functions.php");
 require_once(__DIR__ . "/../../lib/ajax_functions.php");
-require_once "../lib/log.php";
+require_once(__DIR__ . "/../../lib/log.php");
 require_once("../../lib/cc__load_methods.php");
 require_once("../../lib/email__load_methods.php");
 require_once "../lib/email.php";
