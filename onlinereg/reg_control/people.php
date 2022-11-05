9565240f4e0184f987c53c50027bb75c8df5b22c
Syd Weinstein <sydwhv@users.noreply.github.com>
2022-11-05 14:55:32 -0400

    fix not having proper defaults for all fields in add new person

--- onlinereg/reg_control/people.php
+++ onlinereg/reg_control/people.php
@@ -415,6 +415,10 @@ page_init($page,
                 onclick='hideBlock("#addPerson")'>(hide)</a>
             <br/>
             <form id="addPersonForm" name="addPerson" method="POST" action="javascript:void(0)">
+                <input type="hidden" name="share_reg" value="Y"/>
+                <input type="hidden" name="contact_ok" value="Y"/>
+                <input type="hidden" name="active" value="Y"/>
+                <input type="hidden" name="banned" value="N"/>               
                 <table class='formalign'>
                     <tbody id='addPersonFormName'>
                         <tr>
@@ -476,6 +480,16 @@ page_init($page,
                             <td class='formfield' colspan=2><input type="text" name="email" size=30/></td>
                             <td class='formfield' colspan=2><input type="text" name="phone" size=10/></td>
                             <td></td>
+                        </tr>
+                    </tbody>
+                     <tbody id="addPersonFormNotes">
+                        <tr>
+                            <td class='formlabel' colspan=2>Open Notes</td>
+                            <td class='formlabel' colspan=3>Private Notes</td>
+                        </tr>
+                        <tr>
+                            <td class='formfield' colspan=2><textarea rows=5 cols=30 name="open_notes"></textarea></td>
+                            <td class='formfield' colspan=3><textarea rows=5 cols=20 name="admin_notes"></textarea></td>
                         </tr>
                     </tbody>
                     <tfoot id="addPersonFormButtons">
--- onlinereg/reg_control/scripts/addPerson.php
+++ onlinereg/reg_control/scripts/addPerson.php
@@ -18,28 +18,108 @@ if(!isset($_POST)) {
     exit();
 }
 
-$query = "INSERT INTO newperson (last_name, first_name, middle_name, suffix, email_addr, phone, badge_name, address, addr_2, city, state, zip, country) VALUES (";
+$query = <<<EOS
+INSERT INTO newperson (last_name, first_name, middle_name, suffix, email_addr, phone, badge_name,
+    address, addr_2, city, state, zip, country, share_reg_ok, contact_ok)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+EOS;
 
-
-$query .= "'" . sql_safe($_POST['lname']) . "', '" .
-    sql_safe($_POST['fname']) . "', '" .
-    sql_safe($_POST['mname']) . "', '" .
-    sql_safe($_POST['suffix']) . "', ";
-$query .= "'" . sql_safe($_POST['email']) . "', '" .
-    sql_safe($_POST['phone']) . "', ";
-if(isset($_POST['badge']) && $_POST['badge'] != '') {
-  $query .= "'" . sql_safe($_POST['badge']) . "', ";
-} else {
-  $query .= "NULL, ";
+$parmtypes = 'sssssssssssssss';
+$last_name = "";
+if (array_key_exists('lname', $_POST)) {
+    $last_name = trim($_POST['lname']);
+    if (is_null($last_name))
+        $last_name = "";
 }
-$query .= "'" . sql_safe($_POST['address']) . "', '" .
-    sql_safe($_POST['addr2']) . "', '" .
-    sql_safe($_POST['city']) . "', '" .
-    sql_safe($_POST['state']) . "', '" .
-    sql_safe($_POST['zip']) . "', '" .
-    sql_safe($_POST['country']) . "');";
+$first_name = "";
+if (array_key_exists('fname', $_POST)) {
+    $first_name = trim($_POST['fname']);
+    if (is_null($first_name))
+        $first_name = "";
+}
+$middle_name = "";
+if (array_key_exists('mname', $_POST)) {
+    $middle_name = trim($_POST['mname']);
+    if (is_null($middle_name))
+        $middle_name = "";
+}
+$suffix = "";
+if (array_key_exists('suffix', $_POST)) {
+    $suffix = trim($_POST['suffix']);
+    if (is_null($suffix))
+        $suffix = "";
+}
+$email = "";
+if (array_key_exists('email', $_POST)) {
+    $email = trim($_POST['email']);
+    if (is_null($email))
+        $email = "";
+}
+$phone = "";
+if (array_key_exists('phone', $_POST)) {
+    $phone = trim($_POST['phone']);
+    if (is_null($phone))
+        $phone = "";
+}
+$badge = "";
+if (array_key_exists('badge', $_POST)) {
+    $badge = trim($_POST['badge']);
+    if (is_null($badge))
+        $badge = "";
+}
+$address = "";
+if (array_key_exists('address', $_POST)) {
+    $address = trim($_POST['address']);
+    if (is_null($address))
+        $address = "";
+}
+$addr2 = "";
+if (array_key_exists('addr2', $_POST)) {
+    $addr2 = trim($_POST['addr2']);
+    if (is_null($addr2))
+        $addr2 = "";
+}
+$city = "";
+if (array_key_exists('city', $_POST)) {
+    $city = trim($_POST['city']);
+    if (is_null($city))
+        $city = "";
+}
+$state = "";
+if (array_key_exists('state', $_POST)) {
+    $state = trim($_POST['state']);
+    if (is_null($state))
+        $state = "";
+}
+$zip = "";
+if (array_key_exists('zip', $_POST)) {
+    $zip = trim($_POST['zip']);
+    if (is_null($zip))
+        $zip = "";
+}
+$country = "";
+if (array_key_exists('country', $_POST)) {
+    $country = trim($_POST['country']);
+    if (is_null($country))
+        $country = "";
+}
+$share_ok = 'Y';
+if (array_key_exists('share_ok', $_POST)) {
+    $share_ok = trim($_POST['share_ok']);
+    if (is_null($share_ok))
+        $share_ok = "Y";
+}
+$contact_ok = 'Y';
+if (array_key_exists('contact_ok', $_POST)) {
+    $contact_ok = trim($_POST['contact_ok']);
+    if (is_null($contact_ok))
+        $contact_ok = "Y";
+}
 
-$id = dbInsert($query);
+
+$values = array($last_name, $first_name, $middle_name, $suffix, $email, $phone, $badge, $address, $addr2, $city, $state, $zip, $country, $share_ok, $contact_ok);
+
+$id = dbSafeInsert($query, $parmtypes, $values);
 
 $response['id'] = $id;
 
