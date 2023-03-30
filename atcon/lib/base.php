2b0babb9f3519fadd63b45b9bb085da88eab62fc
Syd <63214729+sydwhv@users.noreply.github.com>
2023-03-30 12:05:37 -0400

    remove checkin and regiter, clean up unused functions in atcon.js (comment out) and base.php (remove unused functions that were commented out)

--- atcon/lib/base.php
+++ atcon/lib/base.php
@@ -25,42 +25,6 @@ function isWebRequest()
     return isset($_SERVER) && isset($_SERVER['HTTP_USER_AGENT']);
 }
 
-/*
-function ageDialog($con)
-{
-    $ageListR = callHome("ageList.php", "POST", "con=" . $con['id']);
-    $ageList = json_decode($ageListR,true);
-    //echo "<div>"; var_dump($ageList) ; echo "</div>";
-?>
-<script>
-    var prices = {
-        <?php foreach($ageList as $age) {
-            echo $age['memAge'] . " : " . $age['price'] . ", ";
-        } ?>
-        all : 0}
-</script>
-<div id='getAge' class='dialog'>
-    <form id='getAgeForm' action='javascript:void(0);'>
-        <input type='hidden' id='getAgeBadgeId' />
-        <input type='hidden' id='getAgeBadgeWhich' />
-        <input type='hidden' id='getAgeAction' />
-        <select id='getAgeSelect'>
-            <?php foreach($ageList as $age) {
-                echo "<option value='" . $age['memAge'] . "'>"
-                    . $age['label'] . " ($" . $age['price'] . ")</option>\n";
-            } ?>
-        </select>
-        <input type='submit' id='getAgeSubmit' value='Set Age'
-            onclick='addBadgeAddon($("#getAgeAction").val(),
-                                   $("#getAgeBadgeId").val(),
-                                   $("#getAgeBadgeWhich").val(),
-                                   $("#getAgeSelect").val(), true);
-                     $("#getAge").dialog("close");
-                     return false;' />
-    </form>
-</div>
-<?php }
-*/
 function page_init($title, $tab, $css, $js)
 {
     $con = get_conf('con');
@@ -190,64 +154,6 @@ function page_init($title, $tab, $css, $js)
         <?php
     }
 }
-
-function con_info()
-{
-    $con = get_conf("con");
-##        $count_res = dbQuery("select count(*) from reg where conid='".$con['id']."';");
-##        $badgeCount = fetch_safe_array($count_res);
-##        $count_res = dbQuery("select count(*) from reg where conid='".$con['id']."' AND locked='N';");
-##        $unlockCount = fetch_safe_array($count_res);
-
-    ?>
-    <div id='regInfo'>
-        <span id='regInfoCon' class='left'>
-            Con: <span class='blocktitle'>
-                <?php echo $con['label']; ?>
-            </span>
-            <small>
-                <?php echo "con_info() doesn't work yet";
-##           echo $badgeCount[0] . " Badges (" .
-##                $unlockCount[0] . " Ready)";
-                ?>
-            </small>
-        </span>
-    </div>
-    <?php
-}
-
-/*
-function callHome($script, $method, $data) {
-#print("<br/>" . $script ." :" . strtoupper($method) . " :'". $data . "'<br/>");
-    $access = get_conf('user');
-    $url = $access['server'] . "/" . $script;
-    #error_log($url);
-#print("<br/>server: " . $url . "<br/>");
-    $ch = curl_init($url);
-
-    curl_setopt($ch, CURLOPT_USERPWD, $access['user'] . ":" . $access['passwd']);
-    if(strtoupper($method)=='POST') {
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    }
-
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Length: ' . strlen($data))
-    );
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);
-
-    $result = curl_exec($ch);
-    if($result === false) {
-        print("CURL Error: " . curl_error($ch) . " (" . curl_errno($ch) . ")<br>");
-    }
-
-    curl_close($ch);
-
-    return($result);
-}
-*/
 
 function page_foot($title)
 {
@@ -540,73 +446,7 @@ EOS;
     }
     return in_array($method, $perms);
 }
-/*
-function initReceipt() {
-  $con = get_conf('con');
-  $width = 30;
-  $pad = floor($width/2 + strlen("Receipt")/2);
-  $return = "\n" . sprintf("%${pad}s", "Receipt") . "\n";
-  $pad = floor($width/2 + strlen($con['label'])/2);
-  $return = "\n" . sprintf("%${pad}s", $con['label']) . "\n";
 
-  date_default_timezone_set("America/New_York");
-  $date = date("M j, Y H:m:s");
-  $pad =  floor($width/2 + strlen($date)/2);
-  $return .= sprintf("%${pad}s", $date) . "\n";
-
-  $return .= "\n" . str_repeat('-',$width) . "\n";
-
-    return $return;
-}
-
-function closeReceipt($info) {
-  $width=30;
-
-  $type=$info['type'];
-  $sub = $info['price'];
-  $tax = $info['tax'];
-  $total=$info['withtax'];
-  $amt = $info['amount'];
-  $change = $info['change_due'];
-  $desc = $info['description'];
-  $cc_num = $info['cc'];
-  $cc_code = $info['cc_approval_code'];
-
-  $return = "\n";
-  if($sub>0) {
-  $subStr = sprintf("%01.2f", $sub);
-  $pad = $width - strlen("Subtotal:");
-  $return .= "Subtotal:" . sprintf("%${pad}s",$subStr) . "\n";
-
-  $subTax = sprintf("%01.2f", $tax);
-  $pad = $width - strlen("+ Tax:");
-  $return .= "+ Tax:" . sprintf("%${pad}s",$subTax) . "\n";
-  }
-
-  $subTotal = sprintf("%01.2f", $total);
-  $pad = $width - strlen("Total:");
-  $return .= "Total:" . sprintf("%${pad}s",$subTotal) . "\n";
-
-  $subAmt = sprintf("%01.2f", $amt);
-  $pad = $width - strlen("Payment Received: $type");
-  $return .= "Payment Received" . sprintf("%${pad}s",$subAmt) . "\n";
-  if($type == "check") {
-    $return .= "  - Check #$desc\n";
-  }
-  if($type == "credit") {
-    $return .= "  - Card: " . $cc_num . "\n";
-    $return .= "  - Auth: " . $cc_code . "\n";
-  }
-  $return .= "\n";
-
-  $subCng = sprintf("%01.2f", $change);
-  $pad = $width - strlen("Change:");
-  $return .= "Change:" . sprintf("%${pad}s",$subCng) . "\n";
-  $return .= "\n\n\n\n\n";
-
-  return $return;
-}
-*/
 function RenderErrorAjax($message_error)
 {
     global $return500errors;
@@ -623,25 +463,4 @@ function Render500ErrorAjax($message_error)
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
     echo "$message_error";
 }
-/*
-function passwdForm() {
-?>
-<div id='passwordWrap'>
-    <button id='logout' class='right'
-        onclick='window.location.href=window.location.pathname+"?action=logout"'>
-        Logout
-    </button>
-    <span class='blocktitle' onclick='$("#chpw").toggle()'>Change Password</span>
-    <div id='chpw'>
-        <form action='javascript:void(0)' id='chpwForm'>
-            Current Password: <input type='password' name='passwd' /><br />
-            New Password: <input type='password' name='newpasswd' id='newpw1' /><br />
-            New PW again: <input type='password' id='newpw2' /><br />
-            <input type='submit' onclick='pw_script("#chpwForm");' />
-        </form>
-    </div>
-</div>
-<?php
-}
-*/
 ?>
