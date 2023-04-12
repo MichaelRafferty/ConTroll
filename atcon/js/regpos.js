4f611d460fdd75a045a4c512250e2689c4aef18f
Syd <63214729+sydwhv@users.noreply.github.com>
2023-04-12 18:35:27 -0400

    set dirty flag on rows updated

--- atcon/js/regpos.js
+++ atcon/js/regpos.js
@@ -2250,6 +2250,7 @@ function review_update() {
                 if (cart_perinfo[rownum][field] != el.value) {
                    // alert("updating  row " + rownum + ":" + rownum + ":" + field + " from '" + cart_perinfo[rownum][field] + "' to '" + el.value + "'");
                     cart_perinfo[rownum][field] = el.value;
+                    cart_perinfo[rownum]['dirty'] = true;
                 }
             }
         }
