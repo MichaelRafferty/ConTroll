8a03feaa008d881fa00937b57fd64ccb3af046e6
Syd <63214729+sydwhv@users.noreply.github.com>
2023-10-28 19:12:36 -0400

    fix patch name in log

--- install/Database_Patch_Scripts/p13_reg_history_actions.sql
+++ install/Database_Patch_Scripts/p13_reg_history_actions.sql
@@ -2,3 +2,5 @@
 /* add new actions to track */
 
 ALTER TABLE reg_history MODIFY COLUMN `action` enum('attach','print','notes','transfer','rollover','overpayment','refund') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+INSERT INTO patchLog(id, name) values(13, 'reg_history_actions');
