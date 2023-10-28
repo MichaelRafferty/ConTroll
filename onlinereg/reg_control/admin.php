4150d52fe38b27ad093c2d9958eb3b624fdd15dd
Syd <63214729+sydwhv@users.noreply.github.com>
2023-10-28 11:05:53 -0400

    make memId visible

--- onlinereg/reg_control/admin.php
+++ onlinereg/reg_control/admin.php
@@ -12,7 +12,7 @@ if(!$need_login or !checkAuth($need_login['sub'], $page)) {
 
 page_init($page,
     /* css */ array('https://unpkg.com/tabulator-tables@5.5.2/dist/css/tabulator.min.css',
-                //  'https://unpkg.com/tabulator-tables@5.5.2/dist/css/tabulator_bootstrap5.min.css',
+                    //'https://unpkg.com/tabulator-tables@5.5.2/dist/css/tabulator_bootstrap5.min.css',
                     'css/base.css',
                    ),
     /* js  */ array( //'https://cdn.jsdelivr.net/npm/luxon@3.1.0/build/global/luxon.min.js',
