ef196cbc9f9687447ec5e4efc935a41353ac007f
Syd <63214729+sydwhv@users.noreply.github.com>
2023-10-31 16:20:38 -0400

    upgrade bootstrap to 5.3.2

--- vendor/lib/base.php
+++ vendor/lib/base.php
@@ -34,7 +34,7 @@ echo <<<EOF
     <link href='css/jquery-ui-1.13.1.css' rel='stylesheet' type='text/css' />
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN' crossorigin='anonymous'>
 
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN' crossorigin='anonymous'>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL' crossorigin='anonymous'></script>
     <script type='text/javascript' src='js/jquery-min-3.60.js'></script>
     <script type='text/javascript' src='js/jquery-ui.min-1.13.1.js'></script>
     <script type='text/javascript' src='js/vendor.js'></script>
