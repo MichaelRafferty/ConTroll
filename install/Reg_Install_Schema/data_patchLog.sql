92c7256a4a4f35cef9703011fdcd97c0ee141c06
Syd <63214729+sydwhv@users.noreply.github.com>
2023-12-22 16:20:20 -0500

    fix bad patchLog data table and freeze point on schema

--- install/Reg_Install_Schema/data_patchLog.sql
+++ install/Reg_Install_Schema/data_patchLog.sql
@@ -11,7 +11,11 @@
 
 LOCK TABLES `patchLog` WRITE;
 ALTER TABLE `patchLog` DISABLE KEYS ;
-INSERT INTO `patchLog` VALUES (1,'ATCON Auth Changes','2023-09-21 19:55:19'),(2,'Local Print Servers','2023-09-21 19:58:10'),(3,'Atcon History','2023-09-21 19:59:54'),(4,'Rename BSFS to Club','2023-09-21 20:03:04'),(5,'memList  to DateTime','2023-09-21 20:03:22'),(6,'Foreign Keys','2023-09-21 20:06:43'),(7,'volrollover auth','2023-09-21 20:07:56'),(9,'coupons','2023-09-21 20:15:09'),(10,'oldreg','2023-09-21 20:41:49'),(11,'reg_history','2023-10-27 20:03:26'),(14,'mergePerid_proc','2023-12-09 16:53:25');
+INSERT INTO `patchLog` VALUES (1,'ATCON Auth Changes','2023-09-21 19:55:19'),(2,'Local Print Servers','2023-09-21 19:58:10'),(3,'Atcon History','2023-09-21 19:59:54'),
+                              (4,'Rename BSFS to Club','2023-09-21 20:03:04'),(5,'memList  to DateTime','2023-09-21 20:03:22'),(6,'Foreign Keys','2023-09-21 20:06:43'),
+                              (7,'volrollover auth','2023-09-21 20:07:56'),(8,'new vendor','2023-12-22 21:16:01'),(9,'coupons','2023-09-21 20:15:09'),
+                              (10,'oldreg','2023-09-21 20:41:49'),(11,'reg_history','2023-10-27 20:03:26'),(12,'reg_complete_trans','2023-12-22 21:13:24'),
+                              (13,'reg_history_actions','2023-12-22 21:14:32'),(14,'mergePerid_proc','2023-12-09 16:53:25'),(15,'badgeList_perid','2023-12-22 21:15:31');
 ALTER TABLE `patchLog` ENABLE KEYS ;
 UNLOCK TABLES;
 
--- install/lib/createMissingTables.php
+++ install/lib/createMissingTables.php
@@ -7,6 +7,145 @@ function createMissingTables($options) : int {
     $mysqlConf = get_conf('mysql');
     logEcho('Checking for missing tables/functions/procedures/keys in the database ' . $mysqlConf['db_name']);
 
+    // get list of tables;
+    $tables = [];
+    $sql = "show tables;";
+    $sqlR = dbQuery($sql);
+    while ($row = $sqlR->fetch_row()) {
+        $tables[$row[0]] = true;
+    }
+    $sqlR->free();
+
+    // process the sql scripts in the Reg_Install_Schema directory
+    $dir = new DirectoryIterator('Reg_Install_Schema');
+    $dataLoads = [];
+    $procloads = [];
+
+    // loop over each file
+    foreach ($dir as $entry) {
+        if (!$entry->isFIle())
+            continue;
+
+        $fname = $entry->getFileName();
+        // skip non SQL files
+        if (!str_ends_with($fname, '.sql')) {
+            continue;
+        }
+        // save data loads for last
+        if (str_starts_with($fname, 'data_')) {
+            $dataLoads[] = $fname;
+            continue;
+        }
+        // save procedure/view loads for later
+        if (str_starts_with($fname, 'zz_')) {
+            $procloads[] = $fname;
+            continue;
+        }
+        // skip over the schema load, it's already been handled
+        if ($fname == 'create_reg_schema.sql')
+            continue;
+
+        // ok we have a schema file to process
+        $table = preg_replace('/^[^_]*_(.*)\.sql$/', '\1', $fname);
+
+        if (array_key_exists($table, $tables)) {
+            logEcho("$table exists in the database", true);
+            // future check table for matching schema
+        } else {
+            if (array_key_exists('c', $options)) {
+                $sql = file_get_contents('Reg_Install_Schema/' . $fname);
+                logEcho("Creating $table from $fname");
+                dbMultiQuery($sql);
+                // skip over result sets
+                dbNextResult();
+                dbNextResult();
+                dbNextResult();
+                // PHP mysqli isn't returning a valid result from a DML multi-query, use select to verify it actually worked
+                $sql = <<<EOS
+SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE
+FROM  information_schema.TABLES 
+WHERE TABLE_SCHEMA LIKE ? AND TABLE_TYPE LIKE 'BASE TABLE' AND TABLE_NAME = ?;
+EOS;
+                $sqlR = dbSafeQuery($sql, 'ss', array($mysqlConf['db_name'], $table));
+                if ($sqlR === false || $sqlR->num_rows <= 0) {
+                    logEcho("Unable to create $table");
+                    $errors++;
+                }
+            } else {
+                logEcho("$table is not in the database");
+                $table = preg_replace('/^[^_]*_(.*)\.sql$/', '\1', $fname);
+                $checkSQLR = dbQuery("SELECT count(*) AS occurs FROM $table;");
+                if ($checkSQLR === false) {
+                    logEcho ("Error querying $table for rowcount");
+                    $errors++;
+                }
+                $rowcnt = $checkSQLR->fetch_row()[0];
+                if ($rowcnt > 0) {
+                    logEcho("Skipping table $table as it already has $rowcnt rows of data");
+                    continue;
+                }
+                $sql = file_get_contents('Reg_Install_Schema/' . $fname);
+                logEcho("Loading $table from $fname");
+                dbMultiQuery($sql);
+                // skip over result sets
+                dbNextResult();
+                dbNextResult();
+                dbNextResult();
+                $sql = <<<EOS
+SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE
+FROM  information_schema.TABLES 
+WHERE TABLE_SCHEMA LIKE ? AND TABLE_TYPE LIKE 'BASE TABLE' AND TABLE_NAME = ?;
+EOS;
+                $sqlR = dbSafeQuery($sql, 'ss', array($mysqlConf['db_name'], $table));
+                if ($sqlR === false || $sqlR->num_rows <= 0) {
+                    logEcho("Unable to create $table");
+                    $errors++;
+                }
+
+            }
+        }
+    }
+
+    if ($errors == 0) {
+        if (sizeof($dataLoads) > 0) {
+            logEcho("Creating initial data values");
+            foreach ($dataLoads as $fname) {
+                $table = preg_replace('/^[^_]*_(.*)\.sql$/', '\1', $fname);
+                $checkSQLR = dbQuery("SELECT count(*) AS occurs FROM $table;");
+                if ($checkSQLR === false) {
+                    logEcho ("Error querying $table for rowcount");
+                    $errors++;
+                }
+                $rowcnt = $checkSQLR->fetch_row()[0];
+                if ($rowcnt > 0) {
+                    logEcho("Skipping table $table as it already has $rowcnt rows of data");
+                    continue;
+                }
+                $sql = file_get_contents('Reg_Install_Schema/' . $fname);
+                logEcho("Loading $table from $fname");
+                $set = dbMultiQuery($sql);
+                // skip over result sets
+                dbNextResult();
+                dbNextResult();
+                dbNextResult();
+                dbNextResult();
+                dbNextResult();
+                $checkSQLR = dbQuery("SELECT count(*) AS occurs FROM $table;");
+                if ($checkSQLR === false) {
+                    logEcho ("Error querying $table for rowcount");
+                    $errors++;
+                }
+                $rowcnt = $checkSQLR->fetch_row()[0];
+                if ($rowcnt <= 0) {
+                    logEcho ("Error inserting data into $table");
+                    $errors++;
+                }
+            }
+        }
+
+        echo "need zz_ ";
+    }
+
     logEcho('Completed adding missing items from '. $mysqlConf['db_name']);
     if ($errors > 0)
         logEcho("Errors while adding missing items, cannot continue");
