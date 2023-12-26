7be365b7f7a70dc15153cba4fd47db27dcc25893
Mike Rafferty <mike@bsfs.org>
2023-12-26 22:44:18 +0000

    badge Print step 1

--- atcon/lib/badgePrintFunctions.php
+++ atcon/lib/badgePrintFunctions.php
@@ -1,30 +1,28 @@
 <?php
 require_once("base.php");
 
-$badgeTypes = array(
-    'bsfs'     => 'B',
-    'discount' => 'M',
-    'freebie'  => 'F',
-    'goh'      => 'G',
-    'rollover' => 'R',
-    'standard' => 'M',
-    'yearahead' => 'M',
-    'premium'  => 'M',
-    'test'     => 'X',
-    'Attending' => 'A',
-    'Vendor' => 'V',
-    'voter' => 'V',
-    'NoRights' => 'N'
-);
+$con = get_conf('con');
+$conid = $con['id'];
 
-function lookupType($type)//:string {
-{
-    global $badgeTypes;
-    return $badgeTypes[$type];
+$badgeTypeQ = "SELECT memCategory, badgeLabel FROM memCategories WHERE active='Y';";
+$badgeTypeR = dbQuery($badgeTypeQ);
+$badgeTypes = array();
+
+while($badgeType = $badgeTypeR->fetch_assoc()) {
+$badgeTypes[$badgeType['memCategory']] = $badgeType['badgeLabel'];
+}
+
+$badgeFlagQ = "SELECT ageType, badgeFlag FROM ageList WHERE conid=? and badgeFlag is not null;";
+$badgeFlagR = dbSafeQuery($badgeFlagQ, 'i', array($conid));
+$badgeFlags = array();
+
+while($badgeFlag = $badgeFlagR->fetch_assoc()) {
+$badgeFlags[$badgeFlag['ageType']] = $badgeFlag['badgeFlag'];
 }
 
 function init_file($printer)//:string {
 {
+    global $badgeTypes;
     if ($printer[0] == 'None' && $printer[2] == '') {
         $response['error'] = "You have no printer defined, you cannot print a badge.";
         ajaxSuccess($response);
@@ -71,6 +69,8 @@ function write_badge($badge, $tempfile, $printer):void {
 
 function write_ps($badge, $tempfile)//: void {
 {
+    global $badgeTypes;
+    global $badgeFlags;
     $temp = fopen($tempfile, "a");
     if(!$temp) {
         $response['error'] = "Unable to get open file";
@@ -111,10 +111,8 @@ function write_ps($badge, $tempfile)//: void {
     }
 
     //info line
-    $type = lookupType($badge['category']);
+    $type = $badgeTypes[$badge['category']];
     $id = $badge['id'];
-
-    if($badge['age'] == 'youth') { $type = 'Y'; }
 
     if(strtolower($badge['type'])=='oneday') {
         #$day = date("D");
@@ -131,113 +129,32 @@ function write_ps($badge, $tempfile)//: void {
         . "details setfont\n"
         . "($type $id) show\n\n");
 
-    if($badge['age'] == 'child') {
+    if(array_key_exists($badge['age'], $badgeFlags)) {
+        $flag = $badgeFlags[$badge['age']];
+        $flagLen = mb_strlen($flag);
+        $offset = ceil($flagLen / 2);
+        $wordoffset = floor($flagLen/2);
+        $start = 91 - 8*$offset;
+        $end = 91 + 8*$offset;
+        $word = $start + 4;
+        
         fwrite($temp, "newpath\n"
-            . "68 4 moveto\n"
-            . "68 16 lineto\n"
-            . "114 16 lineto\n"
-            . "114 4 lineto\n"
+            . "$start 4 moveto\n"
+            . "$start 16 lineto\n"
+            . "$end 16 lineto\n"
+            . "$end 4 lineto\n"
             . "closepath fill\n"
             . "1 setgray\n"
-            . "72 6\n"
+            . "$word 6\n"
             . "2 copy moveto\n"
             . "childFont setfont\n"
-            . "(child) show\n\n"
+            . "($flag) show\n\n"
             . "0 setgray\n\n");
-      }
-    if($badge['age'] == 'kit') {
-        fwrite($temp, "newpath\n"
-            . "68 4 moveto\n"
-            . "68 16 lineto\n"
-            . "114 16 lineto\n"
-            . "114 4 lineto\n"
-            . "closepath fill\n"
-            . "1 setgray\n"
-            . "72 6\n"
-            . "2 copy moveto\n"
-            . "childFont setfont\n"
-            . "( kit ) show\n\n"
-            . "0 setgray\n\n");
-      }
+
+    }
 
     #fwrite($temp, "grestore\nshowpage\n%%EOF\n");
     fwrite($temp, "\nshowpage\n");
-    fclose($temp);
-}
-
-function write_se450($badge, $tempfile)//:void {
-{
-    $temp = fopen($tempfile, "a");
-    if(!$temp) {
-        $response['error'] = "Unable to get open file";
-        $response['error_message'] = error_get_last();
-        ajaxSuccess($response);
-        exit();
-    }
-
-    //build badge name
-    if($badge['badge_name'] == "") {
-      $badge['badge_name'] = $badge['full_name'];
-    }
-
-    #$badge_name2 = preg_replace("/#/", "", $badge['badge_name']);
-    $badge_name2 = $badge['badge_name'];
-    $badge_name = html_entity_decode($badge_name2, ENT_QUOTES | ENT_HTML401);
-    $name = $badge_name;
-    $namelen = strlen($name);
-    if($namelen > 16) {
-        $len = strrpos(substr($badge_name,1,16), ' ');
-        if($len === false || $len === 0) { $len = 16; }
-        else { $len +=1; }
-        $name = substr($badge_name, 0, $len);
-        $name2 = substr($badge_name, $len, 22);
-        $name .= "\n"
-            . printmod('lbl', array('7cpi', 'DblHighOFF', 'DblWideOFF'))
-            . $name2;
-    } else { $name .= "\n"; }
-
-    $output = printmod('lbl', array('10cpi', 'DblHighON', 'DblWideON'));
-    $output .= $name;
-    $output .= "\n";
-
-    //info line
-    $type = lookupType($badge['category']);
-    $id = $badge['id'];
-
-    if($badge['age'] == 'youth') {
-        $type='Y';
-    }
-
-    if(strtolower($badge['type'])=='oneday') {
-        #$day = date("D");
-        $day = substr($badge['day'], 0, 3);
-    } else { $day = ""; }
-
-    if($badge['age'] == 'child') {
-        $age = sprintf("%schild%s",
-                printmod('lbl', array('InverseON')),
-                printmod('lbl', array('InverseOFF'))
-                );
-    } else if($badge['age'] == 'kit') {
-        $age = sprintf("%s kit %s",
-                printmod('lbl', array('InverseON')),
-                printmod('lbl', array('InverseOFF'))
-                );
-    } else { $age = ""; }
-
-    $output .= sprintf("%s%3s%s%5s%1s%s\n",
-            printmod('lbl', array('10cpi', 'DblHighON', 'DblWideON')),
-            $day,
-            printmod('lbl', array('10cpi', 'DblHighOFF', 'DblWideOFF')),
-            "", $type, $id);
-
-    $output .= sprintf("%s%11s%s",
-            printmod('lbl', array('10cpi', 'DblHighOFF', 'DblWideOFF')),
-            "", $age);
-
-    $output .= "\f";
-
-    fwrite($temp, $output);
     fclose($temp);
 }
 
