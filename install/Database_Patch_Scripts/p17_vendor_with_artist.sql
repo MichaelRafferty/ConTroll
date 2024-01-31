558f06cda9e14e58ea9b8c396b4b44741600a14a
MichaelRafferty <michael.rafferty65@gmail.com>
2024-01-30 19:13:12 -0500

    Delete install/Database_Patch_Scripts/p17_vendor_with_artist.sql
    cleaning this out of test.  we'll update with the real p17 when we finish it.

--- install/Database_Patch_Scripts/p17_vendor_with_artist.sql
+++
@@ -1,8 +0,0 @@
-/* p16 - vendor_with_artist - Add parent/child relationship, add permissions for artist style, indicate type of space as vendor or artshow
-   Note: vendor = no pre-approval needed, artshow - pre-approval required to allow asking for space
- */
-
-alter table
-
-
-INSERT INTO patchLog(id, name) values(16, 'vendor_with_artist');
