4343ea9b1c19e0d496d49750133733eece44b552
Syd <63214729+sydwhv@users.noreply.github.com>
2024-03-23 13:38:57 -0400

    number patch scripts and make final

--- install/Database_Patch_Scripts/p20_artitems.sql
+++ install/Database_Patch_Scripts/p20_artitems.sql
@@ -1,3 +1,8 @@
+/* p20 - artitems - update to tie to exhibitor using new region year id table
+ */
+
 alter table artItems add column exhibitorRegionYearId INT;
 alter table artItems add constraint `artItems_exhibitorRegionYear_fk` FOREIGN KEY (`exhibitorRegionYearId`) REFERENCES `exhibitorRegionYears` (`id`) ON UPDATE CASCADE;
 
+
+INSERT INTO patchLog(id, name) values(20, 'artitems');
