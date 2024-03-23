096b4ce0dc16c60eb50c92036314dcfc30e121e5
Syd <63214729+sydwhv@users.noreply.github.com>
2024-03-23 15:37:40 -0400

    update schema for email length

--- install/Reg_Install_Schema/reg_newperson.sql
+++ install/Reg_Install_Schema/reg_newperson.sql
@@ -17,7 +17,7 @@ CREATE TABLE `newperson` (
   `middle_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `first_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `suffix` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `email_addr` varchar(254) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email_addr` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `badge_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `legalName` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -35,3 +35,5 @@ CREATE TABLE `newperson` (
   KEY `newperson_transid_fk` (`transid`),
   KEY `newperson_perid_fk` (`perid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
