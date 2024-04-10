ad0767fbdc8d1bd09ef3b6883948549d65a04ddf
Syd <63214729+sydwhv@users.noreply.github.com>
2024-04-10 13:28:08 -0400

    make p24 real

--- install/Database_Patch_Scripts/p24_artSales.sql
+++ install/Database_Patch_Scripts/p24_artSales.sql
@@ -2,3 +2,5 @@
  * changes needed to make sales work and track the things we want to track
  */
 ALTER TABLE artItems MODIFY COLUMN `status`     enum('Entered','Not In Show','Checked In','NFS','BID','Quicksale/Sold','Removed from Show',  'purchased/released','To Auction','Sold Bid Sheet','Sold At Auction', 'Checked Out') DEFAULT 'Entered';
+
+INSERT INTO patchLog(id, name) values(24, 'artsales status');
