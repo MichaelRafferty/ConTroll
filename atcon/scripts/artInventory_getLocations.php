78ac7f8e3e05bae4fe6a4a7281d46655663f7a6f
Mike Rafferty <mike@bsfs.org>
2024-04-10 00:54:19 +0000

    artInventory working except for the ability to sell from the auction

--- atcon/scripts/artInventory_getLocations.php
+++ atcon/scripts/artInventory_getLocations.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/../lib/base.php";
 
-$response = array("post" => $_POST, "get" => $_GET, "session" => $_SESSION);
+$response = array("post" => $_POST, "get" => $_GET); //, "session" => $_SESSION);
 
 $con = get_con();
 $conid=$con['id'];
@@ -12,21 +12,40 @@ if($check_auth == false) {
 }
 
 
+$region = '';
+
+if(!array_key_exists('region', $_GET)) {
+    ajaxError("No Region");
+} else {
+    $region = $_GET['region'];
+}
+
 $locQ = <<<EOS
-SELECT art_key, a_panel_list, p_panel_list, a_table_list
-FROM artshow
-WHERE conid=?;
+SELECT exhibitorNumber, eS.locations
+FROM exhibitorRegionYears eRY
+    JOIN exhibitorYears eY ON eY.id=eRY.exhibitorYearId
+    JOIN exhibitorSpaces eS on eS.exhibitorRegionYear=eRY.id
+    JOIN exhibitsRegionYears xRY on xRY.id=eRY.exhibitsRegionYearId
+    JOIN exhibitsRegions xR on xR.id=xRY.exhibitsRegion
+WHERE eY.conid=? and eRY.exhibitorNumber is not null and xR.shortName=?;
 EOS;
-$locR = dbSafeQuery($locQ, 'i', array($conid));
+
+$locR = dbSafeQuery($locQ, 'ii', array($conid, $region));
 
 $locations = array();
 while($loc = fetch_safe_assoc($locR)) {
-    $locations[$loc['art_key']] = array_merge(
-        explode(',', $loc['a_panel_list']),
-        explode(',', $loc['p_panel_list']),
-        explode(',', $loc['a_table_list']),
-    ); 
+    if(!array_key_exists($loc['exhibitorNumber'], $locations)) {
+        $locations[$loc['exhibitorNumber']] = array();
+    }
+    $locations[$loc['exhibitorNumber']] = array_merge($locations[$loc['exhibitorNumber']], explode(',',$loc['locations']));
 }
 
-ajaxSuccess($locations);
+foreach($locations as $key => $value) {
+    sort($value, SORT_NATURAL);
+    $locations[$key] = $value;
+}
+
+$response['locations'] = $locations;
+
+ajaxSuccess($response);
 ?>
