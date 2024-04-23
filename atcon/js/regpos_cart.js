1497f2e9b19f6a0f160575a21581fae4fe8edf30
Syd <63214729+sydwhv@users.noreply.github.com>
2024-04-23 14:37:47 -0400

    restore deleted line

--- atcon/js/regpos_cart.js
+++ atcon/js/regpos_cart.js
@@ -65,6 +65,7 @@ class regpos_cart {
         filt_cat = new Array('upgrade')
         filt_shortname_regexp = null;
         var match = memList.filter(mem_filter);
+        var nonday = 0;
         for (row in match) {
             var label = match[row]['label'];
             var day = label.replace(/.*upgrade +(...).*/i, '$1').toLowerCase();
