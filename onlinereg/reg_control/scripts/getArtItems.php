c2d2b71a0e3e6a0a03fe251d25352fcf92c85f8f
Syd <63214729+sydwhv@users.noreply.github.com>
2024-04-23 08:20:14 -0400

    fix group by issue with no agg to a select distinct

--- onlinereg/reg_control/scripts/getArtItems.php
+++ onlinereg/reg_control/scripts/getArtItems.php
@@ -47,15 +47,14 @@ $items=array();
     }
 
 $artistQ = <<<EOS
-SELECT e.exhibitorName, ery.id as exhibitorRegionYearId, ery.exhibitorNumber
+SELECT DISTINCT e.exhibitorName, ery.id as exhibitorRegionYearId, ery.exhibitorNumber
 FROM exhibitorYears ey 
     JOIN exhibitorRegionYears ery ON ery.exhibitorYearId = ey.id
     JOIN exhibitors e ON ey.exhibitorId=e.id
     JOIN exhibitsRegionYears exRY ON exRY.id=ery.exhibitsRegionYearId
     JOIN exhibitorSpaces S on S.exhibitorRegionYear=ery.id 
 WHERE ey.conid=? and exRY.exhibitsRegion=? and S.item_purchased is not null
-    and ery.exhibitorNumber is not null
-GROUP BY e.exhibitorName;
+    and ery.exhibitorNumber is not null;
 EOS; 
 
 $response['art'] = $items;
