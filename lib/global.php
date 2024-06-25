4cad2dd8c8af9936fd83d0769f5d303b21554cae
Syd <63214729+sydwhv@users.noreply.github.com>
2024-06-25 12:33:23 -0400

    upgrade versions - prepare for oauth 2 client

--- lib/global.php
+++ lib/global.php
@@ -37,9 +37,9 @@ EOS;
 // getTabulatorIncludes - returns CDN string for Tabulator
 function getTabulatorIncludes(): array {
     return ( [
-        'tabcss' => 'https://unpkg.com/tabulator-tables@6.2.0/dist/css/tabulator.min.css',
-        'tabbs5' => 'https://unpkg.com/tabulator-tables@6.2.0/dist/css/tabulator_bootstrap5.min.css',
-        'tabjs' => 'https://unpkg.com/tabulator-tables@6.2.0/dist/js/tabulator.min.js',
+        'tabcss' => 'https://unpkg.com/tabulator-tables@6.2.1/dist/css/tabulator.min.css',
+        'tabbs5' => 'https://unpkg.com/tabulator-tables@6.2.1/dist/css/tabulator_bootstrap5.min.css',
+        'tabjs' => 'https://unpkg.com/tabulator-tables@6.2.1/dist/js/tabulator.min.js',
         'luxon' => 'https://cdn.jsdelivr.net/npm/luxon@3.4.4/build/global/luxon.min.js',
         'bs5css' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous",
         'bs5js' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz' crossorigin='anonymous",
