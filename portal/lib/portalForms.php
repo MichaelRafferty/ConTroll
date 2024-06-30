30da2a6dd34ff6a325db8f35cf24433bf864df70
Syd <63214729+sydwhv@users.noreply.github.com>
2024-06-30 19:57:50 -0400

    message change request

--- portal/lib/portalForms.php
+++ portal/lib/portalForms.php
@@ -350,7 +350,7 @@ function drawManagedPerson($personId, $personType, $person, $memberships, $showI
             </button>
 <?php } ?>
             <button class='btn btn-sm btn-primary p-1' style='--bs-btn-font-size: 80%;' onclick="portal.addMembership(<?php echo $person['id'] . ",'" . $person['personType'] . "'"; ?>);">
-                Add/Upgrade Edit Memberships
+                Add/Edit Cart
             </button>
         </div>
     </div>
