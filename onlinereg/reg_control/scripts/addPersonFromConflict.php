6127c9fd4c48a9dcab8788a9284952ba89d70b9a
Syd <63214729+sydwhv@users.noreply.github.com>
2024-06-30 21:21:40 -0400

    fix conflict resolution new to use proper order of args and user_perid

--- onlinereg/reg_control/scripts/addPersonFromConflict.php
+++ onlinereg/reg_control/scripts/addPersonFromConflict.php
@@ -25,29 +25,33 @@ if(!isset($_POST) || !isset($_POST['newID'])) {
 $newPersonQ = <<<EOQ
 INSERT INTO perinfo (last_name, first_name, middle_name, suffix
     , email_addr, phone, badge_name, address, addr_2, city, state, zip
-    , country, contact_ok, share_reg_ok, active, banned,updatedBy)
+    , country, contact_ok, share_reg_ok, active, banned, updatedBy)
 SELECT last_name, first_name, middle_name, suffix, email_addr, phone
     , badge_name, address, addr_2, city, state, zip, country, contact_ok, share_reg_ok, 'Y', 'N', ?
 FROM newperson
 WHERE id = ?;
 EOQ;
 
-$id = dbSafeInsert($newPersonQ, "ii", array($_POST['newID'], $_SESSION['user_id']));
-$rows = dbSafeCmd("UPDATE newperson SET perid=?, updatedBy = ? WHERE id=?;", 'iii', array($id, $_SESSION['user_id'], $_POST['newID']));
-$rows = dbSafeCmd("UPDATE reg SET perid=? WHERE newperid=?;", 'ii', array($id, $_POST['newID']));
-$rows = dbSafeCmd("UPDATE transaction SET perid=? WHERE newperid=?;", 'ii', array($id, $_POST['newID']));
-$rows = dbSafeCmd("UPDATE exhibitors SET perid=? WHERE newperid=?;", 'ii', array($id, $_POST['newID']));
-$rows = dbSafeCmd("UPDATE memberInterests SET perid=? WHERE newperid=?;", 'ii', array($id, $_POST['newID']));
-$rows = dbSafeCmd("UPDATE payorPlans SET perid=? WHERE newperid=?;", 'ii', array($id, $_POST['newID']));
+$id = dbSafeInsert($newPersonQ, "ii", array($_SESSION['user_perid'], $_POST['newID']));
+if ($id !== false) {
+    $rows = dbSafeCmd("UPDATE newperson SET perid=?, updatedBy = ? WHERE id=?;", 'iii', array ($id, $_SESSION['user_id'], $_POST['newID']));
+    $rows = dbSafeCmd("UPDATE reg SET perid=? WHERE newperid=?;", 'ii', array ($id, $_POST['newID']));
+    $rows = dbSafeCmd("UPDATE transaction SET perid=? WHERE newperid=?;", 'ii', array ($id, $_POST['newID']));
+    $rows = dbSafeCmd("UPDATE exhibitors SET perid=? WHERE newperid=?;", 'ii', array ($id, $_POST['newID']));
+    $rows = dbSafeCmd("UPDATE memberInterests SET perid=? WHERE newperid=?;", 'ii', array ($id, $_POST['newID']));
+    $rows = dbSafeCmd("UPDATE payorPlans SET perid=? WHERE newperid=?;", 'ii', array ($id, $_POST['newID']));
 
-$perQ = <<<EOQ
-SELECT banned, concat_ws(' ', first_name, middle_name, last_name) as full_name, email_addr
-    , address, addr_2, concat_ws(' ', city, state, zip) as locale, badge_name, id
-FROM perinfo where id = ?;
+    $perQ = <<<EOQ
+SELECT banned, CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name, email_addr
+    , address, addr_2, CONCAT_WS(' ', city, state, zip) AS locale, badge_name, id
+FROM perinfo WHERE id = ?;
 EOQ;
 
-$response['id'] = $id;
-$response['results'] = dbSafeQuery($perQ, 'i', array($id))->fetch_assoc();
+    $response['id'] = $id;
+    $response['results'] = dbSafeQuery($perQ, 'i', array ($id))->fetch_assoc();
+} else {
+    $response['error'] = "Insert Error";
+}
 
 ajaxSuccess($response);
 ?>
