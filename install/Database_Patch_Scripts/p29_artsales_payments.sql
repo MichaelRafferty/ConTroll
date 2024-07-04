ac9dc19f99a48919d448eeff2062b52bb1029977
Syd <63214729+sydwhv@users.noreply.github.com>
2024-07-04 17:00:21 -0400

    fix past data with patch

--- install/Database_Patch_Scripts/p29_artsales_payments.sql
+++ install/Database_Patch_Scripts/p29_artsales_payments.sql
@@ -4,6 +4,8 @@
 ALTER TABLE payments ADD COLUMN pretax decimal(8,2) DEFAULT NULL AFTER source;
 ALTER TABLE payments ADD COLUMN tax decimal(8,2) DEFAULT 0.00 AFTER pretax;
 
-UPDATE payments SET tax = 0, pretax = amount;
+UPDATE payments SET tax = 0, pretax = amount WHERE IFNULL(tax, 0) = 0 OR pretax is NULL;
+UPDATE transaction SET tax = 0 where tax is NULL;
+update transaction SET withtax = price + tax;
 
 INSERT INTO patchLog(id, name) values(29, 'artsales_payments');
