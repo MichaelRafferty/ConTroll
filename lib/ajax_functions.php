b013d3a10c59f4d4c8cf0cd1487825fa23b78071
Syd <63214729+sydwhv@users.noreply.github.com>
2024-07-07 22:00:53 -0400

    add in the invalid utf8 subs

--- lib/ajax_functions.php
+++ lib/ajax_functions.php
@@ -3,12 +3,12 @@
 function ajaxError($msg) {
     header('HTTP/1.1 401 Unauthorized');
     header('content-type: application/json; charset=utf-8');
-    die(json_encode(array('message' => $msg)));
+    die(json_encode(array('message' => $msg), JSON_INVALID_UTF8_SUBSTITUTE));
 }
 
 function ajaxSuccess($msg) {
     header('Content-Type: application/json; charset=utf-8');
-    print json_encode($msg);
+    print json_encode($msg, JSON_INVALID_UTF8_SUBSTITUTE);
 }
 
 ?>
