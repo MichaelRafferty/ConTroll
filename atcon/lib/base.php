0d9152cf4aa75f9542742429b860afca9463ae64
Syd <63214729+sydwhv@users.noreply.github.com>
2024-07-10 13:39:42 -0400

    fix generic printer select

--- atcon/lib/base.php
+++ atcon/lib/base.php
@@ -568,7 +568,7 @@ EOS;
         if (mb_substr($printer['printerType'], 0, 7) == 'generic') $genericFound = true;
         if (mb_substr($printer['printerType'], 0, 7) == 'receipt') $receiptFound = true;
     }
-    mysqli_free_result($printerR);
+    $printerR->free();
 
     $html = <<<EOS
     <div class='row'>
@@ -629,7 +629,7 @@ EOS;
 EOS;
         foreach ($printers as $key => $printer) {
             if (mb_substr($printer['printerType'], 0, 7) == 'generic') {
-                '<option value="' . escape_quotes($key) . ':::' . escape_quotes($printer['address']) .
+                $html .= '<option value="' . escape_quotes($key) . ':::' . escape_quotes($printer['address']) .
                 ':-:' . escape_quotes($printer['printerName']) . ':-:' . escape_quotes($printer['printerType']) .
                 ':-:' . escape_quotes($printer['codePage']) . '">' . $key . "</option>\n";
             }
