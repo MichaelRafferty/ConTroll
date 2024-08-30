6e0ea764d63fc2087aa019a671a19967a496d10d
Syd <63214729+sydwhv@users.noreply.github.com>
2024-08-30 17:09:55 -0400

    rules now go into the database

--- controll/scripts/regadmin_updateRules.php
+++ controll/scripts/regadmin_updateRules.php
@@ -10,13 +10,10 @@ $response = array("post" => $_POST, "get" => $_GET, "perm"=>$perm);
 
 if ($check_auth == false || !checkAuth($check_auth['sub'], $perm)) {
     $response['error'] = 'Authentication Failed';
+    $response['error'] = 'Authentication Failed';
     ajaxSuccess($response);
     exit();
 }
-
-$con = get_conf('con');
-$conid=$con['id'];
-$nextconid=$conid + 1;
 
 //var_error_log($_POST);
 
@@ -36,7 +33,188 @@ try {
     exit();
 }
 
-$response['warn'] = 'NOT YET';
+// loop over the rules, updating the data and deleting the rule steps as needed
+$dR = <<<EOS
+DELETE FROM memRules
+WHERE name = ?;
+EOS;
+$dRI = <<<EOS
+DELETE FROM memRuleItems
+WHERE name = ?;
+EOS;
+$dRIsingle = <<<EOS
+DELETE FROM memRuleItems
+WHERE name = ? and step = ?;
+EOS;
 
+$numdel = 0;
+$numupd = 0;
+$numins = 0;
+foreach ($rules as $name => $rule) {
+    if (array_key_exists('to_delete', $rule) && $rule['to_delete'] == 1) {
+        $numdel += dbSafeCmd($dRI, 's', array($rule['origName']));
+        $numdel += dbSafeCmd($dR, 's', array($rule['origName']));
+    } else {
+        $ruleItems = $rule['ruleset'];
+        foreach ($ruleItems as $ruleItem) {
+            if (array_key_exists('to_delete', $ruleItem) && $ruleItem['to_delete'] == 1) {
+                $numdel += dbSafeCmd($dRIsingle, 'si', array($ruleItem['origName'], $ruleItem['origStep']));
+            }
+        }
+    }
+}
+
+$uR = <<<EOS
+UPDATE memRules
+SET name = ?, optionName = ?, description = ?, typeList = ?, catList = ?, ageList = ?, memList = ?
+WHERE name = ?;
+EOS;
+$uRI = <<<EOS
+UPDATE memRuleItems 
+SET step = ?, ruleType = ?, applyTo = ?, typeList = ?, catList = ?, ageList = ?, memList = ?
+WHERE name = ? AND step = ?;
+EOS;
+
+// ok, all the deletes are now done, do the updates next
+foreach ($rules as $name => $rule) {
+    if (array_key_exists('to_delete', $rule) && $rule['to_delete'] == 1)
+        continue; // don't update delete lines
+    if (is_numeric($rule['origName']) && $rule['origName'] < 0)
+        continue; // don't update inserts
+
+    // we have a row we can update
+    $optionName = '';
+    if (array_key_exists('optionName', $rule) && $rule['optionName'] != null)
+        $optionName = $rule['optionName'];
+
+    $description = '';
+    if (array_key_exists('description', $rule) && $rule['description'] != null)
+        $description = $rule['description'];
+    
+    $typeList = null;
+    if (array_key_exists('typeList', $rule) && $rule['typeList'] != '')
+        $typeList = $rule['typeList'];
+
+    $catList = null;
+    if (array_key_exists('catList', $rule) && $rule['catList'] != '')
+        $catList = $rule['catList'];
+
+    $ageList = null;
+    if (array_key_exists('ageList', $rule) && $rule['ageList'] != '')
+        $ageList = $rule['ageList'];
+
+    $memList = null;
+    if (array_key_exists('memList', $rule) && $rule['memList'] != '')
+        $memList = $rule['memList'];
+    
+    $numupd += dbSafeCmd($uR, 'ssssssss', array($rule['name'], $optionName, $description, $typeList, $catList, $ageList, $memList, $rule['origName']));
+
+    $ruleItems = $rule['ruleset'];
+    foreach ($ruleItems as $ruleItem) {
+        if (array_key_exists('to_delete', $ruleItem) && $ruleItem['to_delete'] == 1)
+            continue; // don't update delete lines
+        if ($ruleItem['origStep'] < 0) // don't update insert items
+            continue;
+
+        $typeList = null;
+        if (array_key_exists('typeList', $ruleItem) && $ruleItem['typeList'] != '')
+            $typeList = $ruleItem['typeList'];
+
+        $catList = null;
+        if (array_key_exists('catList', $ruleItem) && $ruleItem['catList'] != '')
+            $catList = $ruleItem['catList'];
+
+        $ageList = null;
+        if (array_key_exists('ageList', $ruleItem) && $ruleItem['ageList'] != '')
+            $ageList = $ruleItem['ageList'];
+
+        $memList = null;
+        if (array_key_exists('memList', $ruleItem) && $ruleItem['memList'] != '')
+            $memList = $ruleItem['memList'];
+        
+        $numupd += dbSafeCmd($uRI, 'isssssssi', array($ruleItem['step'], $ruleItem['ruleType'], $ruleItem['applyTo'],
+            $typeList, $catList, $ageList, $memList, $rule['name'], $ruleItem['origStep']));
+    }
+}
+
+// last all the inserts
+$iR = <<<EOS
+INSERT into memRules(name, optionName, description, typeList, catList, ageList, memList) 
+VALUES (?, ? ,?, ?, ?, ?, ?);
+EOS;
+$iRI = <<<EOS
+INSERT into memRuleItems(name, step, ruleType, applyTo, typeList, catList, ageList, memList)   
+VALUES (?, ? ,?, ?, ?, ?, ?, ?);
+EOS;
+
+foreach ($rules as $name => $rule) {
+    if (array_key_exists('to_delete', $rule) && $rule['to_delete'] == 1)
+        continue; // don't update delete lines
+    if (is_numeric($rule['origName']) && $rule['origName'] < 0) {
+        // do insert
+        $optionName = '';
+        if (array_key_exists('optionName', $rule) && $rule['optionName'] != null)
+            $optionName = $rule['optionName'];
+
+        $description = '';
+        if (array_key_exists('description', $rule) && $rule['description'] != null)
+            $description = $rule['description'];
+
+        $typeList = null;
+        if (array_key_exists('typeList', $rule) && $rule['typeList'] != '')
+            $typeList = $rule['typeList'];
+
+        $catList = null;
+        if (array_key_exists('catList', $rule) && $rule['catList'] != '')
+            $catList = $rule['catList'];
+
+        $ageList = null;
+        if (array_key_exists('ageList', $rule) && $rule['ageList'] != '')
+            $ageList = $rule['ageList'];
+
+        $memList = null;
+        if (array_key_exists('memList', $rule) && $rule['memList'] != '')
+            $memList = $rule['memList'];
+
+        $inskey = dbSafeInsert($iR, 'sssssss', array($rule['name'], $optionName, $description,
+            $typeList, $catList,  $ageList, $memList));
+        if ($inskey)
+            $numins++;
+        }
+
+    $ruleItems = $rule['ruleset'];
+    foreach ($ruleItems as $ruleItem) {
+        if (array_key_exists('to_delete', $ruleItem) && $ruleItem['to_delete'] == 1)
+            continue; // don't update delete lines
+        if ($ruleItem['origStep'] < 0) {
+            // new row do the insert
+            $typeList = null;
+            if (array_key_exists('typeList', $ruleItem) && $ruleItem['typeList'] != '')
+                $typeList = $ruleItem['typeList'];
+
+            $catList = null;
+            if (array_key_exists('catList', $ruleItem) && $ruleItem['catList'] != '')
+                $catList = $ruleItem['catList'];
+
+            $ageList = null;
+            if (array_key_exists('ageList', $ruleItem) && $ruleItem['ageList'] != '')
+                $ageList = $ruleItem['ageList'];
+
+            $memList = null;
+            if (array_key_exists('memList', $ruleItem) && $ruleItem['memList'] != '')
+                $memList = $ruleItem['memList'];
+            
+            $inskey = dbSafeInsert($iRI, 'sissssss', array ($rule['name'], $ruleItem['step'], $ruleItem['ruleType'], $ruleItem['applyTo'],
+                $typeList, $catList, $ageList, $memList));
+            if ($inskey)
+                $numins++;
+        }
+    }
+}
+
+$response['success'] = 'All rules updated';
+$response['numins'] = $numins;
+$response['numupd'] = $numupd;
+$response['numdel'] = $numdel;
 ajaxSuccess($response);
 ?>
