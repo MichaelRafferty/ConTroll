860029c60ce0cf052f9b14ae633303c2c64b2277
Syd <63214729+sydwhv@users.noreply.github.com>
2024-09-16 19:02:41 -0400

    forgot to push version change

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -17,10 +17,10 @@ function getTabulatorIncludes(): array {
 
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
-$portalJSVersion = '0.3.16';
-$libJSversion = '0.3.16';
-$controllJSversion = '0.3.16';
-$globalJSversion = '0.3.16';
+$portalJSVersion = '0.3.17';
+$libJSversion = '0.3.17';
+$controllJSversion = '0.3.17';
+$globalJSversion = '0.3.17';
 $atJSversion = '0.3.8';
-$exhibitorJSversion = '0.3.8';
+$exhibitorJSversion = '0.3.9';
 $onlineregJSversion = '0.3.8';
