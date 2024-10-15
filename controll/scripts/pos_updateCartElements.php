bd7b265621cd5030c00fe130bb019d750338f692
Syd <63214729+sydwhv@users.noreply.github.com>
2024-10-15 15:58:44 -0400

    first pass at testing, can update perinfo and memberships

--- controll/scripts/pos_updateCartElements.php
+++ controll/scripts/pos_updateCartElements.php
@@ -1,8 +1,9 @@
 <?php
-// library AJAX Processor: reg_updateCartElements.php
-// Balticon Registration System
+// library AJAX Processor: pos_updateCartElements.php
+// ConTroll Registration System
 // Author: Syd Weinstein
 // Store the cart into the system using add/update/delete and create appropriate transaction records
+// Used both by mail in registration (controll/registration.php) and atcon (atcon/regpos.php)
 
 require_once '../lib/base.php';
 
@@ -43,10 +44,6 @@ if (sizeof($cart_perinfo) <= 0) {
     ajaxError('No members are in the cart');
     return;
 }
-
-$updated_perinfo = [];
-$updated_membership = [];
-$update_permap = [];
 $error_message = '';
 
 $per_ins = 0;
@@ -64,119 +61,39 @@ INSERT INTO perinfo(last_name,first_name,middle_name,suffix,legalName,pronouns,e
                     open_notes,banned,active,contact_ok,creation_date,updatedBy)
 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,'N','Y','Y',now(),?);
 EOS;
-$existingQ = <<<EOS
-SELECT last_name, first_name, middle_name, suffix, legalName, pronouns, email_addr, phone, badge_name, address, addr_2, city, state, zip, country,
-       open_notes, change_notes
-FROM perinfo
-WHERE id = ?;
-EOS;
+$insPDt = 'ssssssssssssssssi';
+
 $updPerinfoSQL = <<<EOS
 UPDATE perinfo SET
-    last_name=?,first_name=?,middle_name=?,suffix=?,legalName=?,pronouns=>,email_addr=?,phone=?,badge_name=?,address=?,addr_2=?,city=?,state=?,zip=?,country=?,
-    open_notes=?,banned='N',update_date=NOW(),active='Y',change_notes=?,updatedBy=?
+    last_name=?,first_name=?,middle_name=?,suffix=?,legalName=?,pronouns=?,email_addr=?,phone=?,badge_name=?,address=?,addr_2=?,city=?,state=?,zip=?,country=?,
+    open_notes=?,banned='N',update_date=NOW(),active='Y',updatedBy=?
 WHERE id = ?;
 EOS;
+$updPDt = 'ssssssssssssssssii';
+
 $insRegSQL = <<<EOS
 INSERT INTO reg(conid,perid,price,couponDiscount,paid,create_user,create_trans,memId,coupon,create_date,status)
 VALUES (?,?,?,?,?,?,?,?,?,now(),?);
 EOS;
+$insRDt = 'iidddiiiis';
+
 $updRegSQL = <<<EOS
-UPDATE reg SET price=?,couponDiscount=?,paid=?,status = ?, memId=?,coupon=?,updatedBy=?,change_date=now()
+UPDATE reg SET price=?,couponDiscount=?,paid=?, memId=?,coupon=?,updatedBy=?,change_date=now()
 WHERE id = ?;
 EOS;
+$updRDt = 'dddiiii';
+
 $delRegSQL = <<<EOS
 DELETE FROM reg
 WHERE id = ?;
 EOS;
+$delRDt = 'i';
+
 $insHistory = <<<EOS
 INSERT INTO regActions(userid, tid, regid, action, notes)
 VALUES (?, ?, ?, ?, ?);
 EOS;
-// loop over all perinfo records
-for ($row = 0; $row < sizeof($cart_perinfo); $row++) {
-    $cartrow = $cart_perinfo[$row];
-    if (array_key_exists('open_notes', $cartrow)) {
-        $open_notes = $cartrow['open_notes'];
-        if ($open_notes == '')
-            $open_notes = null;
-    } else
-        $open_notes = null;
-
-    $legalName = $cartrow['legalName'];
-    if ($legalName == '') {
-        $legalName = trim($cartrow['first_name']  . ($cartrow['middle_name'] == '' ? ' ' : ' ' . $cartrow['middle_name'] . ' ' ) .
-            $cartrow['last_name'] . ' ' . $cartrow['suffix']);
-    }
-
-    // remove l-r from phone
-    $cartrow['phone'] = trim(removeLROveride($cartrow['phone']));
-
-    if ($cartrow['perid'] <= 0) {
-        // insert this row
-        $paramarray = array(
-            $cartrow['last_name'],$cartrow['first_name'],$cartrow['middle_name'],$cartrow['suffix'],$legalName,$cartrow['pronouns'],
-            $cartrow['email_addr'],$cartrow['phone'],$cartrow['badge_name'],
-            $cartrow['address_1'],$cartrow['address_2'],$cartrow['city'],$cartrow['state'],$cartrow['postal_code'],$cartrow['country'],
-            $open_notes,$_SESSION['user_id']
-        );
-
-        $typestr = 'ssssssssssssssssi';
-        $new_perid = dbSafeInsert($insPerinfoSQL, $typestr, $paramarray);
-        if ($new_perid === false) {
-            $error_message .= "Insert of person $row failed<BR/>";
-        } else {
-            $updated_perinfo[] = array('rowpos' => $row, 'perid' => $new_perid);
-            $update_permap[$cartrow['perid']] = $new_perid;
-            $cart_perinfo[$row]['perid'] = $new_perid;
-            $cartrow['perid'] = $new_perid;
-            $per_ins++;
-        }
-    } else {
-        // update the row
-        // first build the change log
-        $existingR = dbSafeQuery($existingQ, 'i', array($cartrow['perid']));
-        if ($existingR === false || $existingR->num_rows != 1) {
-            $response['status'] = 'error';
-            $response['error'] = "Unable to update cart row: " . $cartrow['perid'] . '/' . $cartrow['last_name'] . ', ' . $cartrow['first_name'];
-            ajaxSuccess($response);
-            return;
-        }
-        $existingP = $existingR->fetch_assoc();
-        $new_change_notes = $existingP['change_notes'];
-        if ($new_change_notes === null)
-            $new_change_notes = '';
-        // build additional items on new change notes
-        $changes = '';
-        foreach ($existingP as $field => $value) {
-            if ($field == 'perid' || $field == 'change_notes')
-                continue;
-
-            if ($field == 'zip')
-                $cartfield = 'postal_code';
-            else if ($field == 'address')
-                $cartfield = 'address_1';
-            else if ($field == 'addr_2')
-                $cartfield = 'address_2';
-            else
-                $cartfield = $field;
-
-            if ($value != $cartrow[$cartfield]) {
-                $changes .= $field . ' updated "' . $value . '" => "' . $cartrow[$cartfield] . '"' . "\n";
-            }
-        }
-        if ($changes != '')
-            $new_change_notes = "\ncontroll/registration Updated " . date(DATE_RFC2822) . " by $user_perid:\n$changes\n" . $new_change_notes;
-
-        $paramarray = array(
-            $cartrow['last_name'],$cartrow['first_name'],$cartrow['middle_name'],$cartrow['suffix'],$legalName,$cartrow['email_addr'],$cartrow['phone'],$cartrow['badge_name'],
-            $cartrow['address_1'],$cartrow['address_2'],$cartrow['city'],$cartrow['state'],$cartrow['postal_code'],$cartrow['country'],$open_notes,
-            $cartrow['contact_ok'],$cartrow['share_reg_ok'],$new_change_notes,$user_id,
-            $cartrow['perid']
-        );
-        $typestr = 'ssssssssssssssssssii';
-        $per_upd += dbSafeCmd($updPerinfoSQL, $typestr, $paramarray);
-    }
-}
+$insHDt = 'iiiss';
 
 // create the controlling transaction, in case the master perinfo needed insertion
 $master_perid = $cart_perinfo[0]['perid'];
@@ -187,64 +104,117 @@ VALUES (?,?,?,?,?,?,now());
 EOS;
 // now insert the master transaction
 $paramarray = array($conid, $master_perid, $user_perid, 0, 0, $tran_type);
-$typestr = 'iiisss';
+$typestr = 'iiidds';
 $master_transid = dbSafeInsert($insTransactionSQL, $typestr, $paramarray);
 if ($master_transid === false) {
     ajaxError('Unable to create master transaction');
     return;
 }
-// now insert/update all reg records and compute the transaction price and paid fields
-for ($row = 0; $row < sizeof($cart_membership); $row++) {
-    $cartrow = $cart_membership[$row];
-    if (!array_key_exists('todelete', $cartrow)) {
-        if ($cartrow['price'] == '')
-            $cartrow['price'] = 0;
-        $total_price += $cartrow['price'];
 
-        if ($cartrow['paid'] == '')
-            $cartrow['paid'] = 0;
-        $total_paid += $cartrow['paid'];
+// loop over all perinfo records
+for ($row = 0; $row < sizeof($cart_perinfo); $row++) {
+    $cartrow = $cart_perinfo[$row];
+    if (array_key_exists('open_notes', $cartrow)) {
+        $open_notes = $cartrow['open_notes'];
+        if ($open_notes == '')
+            $open_notes = null;
+    } else
+        $open_notes = null;
+
+    if ($cartrow['legalName'] == '') {
+        $cartrow['legalName'] = trim($cartrow['first_name']  . ($cartrow['middle_name'] == '' ? ' ' : ' ' . $cartrow['middle_name'] . ' ' ) .
+            $cartrow['last_name'] . ' ' . $cartrow['suffix']);
     }
-    if (!array_key_exists('regid', $cartrow) || $cartrow['regid'] <= 0) {
-        // insert the membership
-        if ($cartrow['perid'] <= 0) {
-            $cartrow['perid'] = $update_permap[$cartrow['perid']];
-        }
-        $paramarray = array($cartrow['conid'], $cartrow['perid'], $cartrow['price'],
-                            $cartrow['price'] > $cartrow['paid'] ? 'unpaid' : 'paid',
-                            $cartrow['paid'], $user_perid, $master_transid, $cartrow['memId']);
-        $typestr = 'iiddsiii';
-        $new_regid = dbSafeInsert($insRegSQL, $typestr, $paramarray);
-        if ($new_regid === false) {
-            $error_message .= "Insert of membership $row failed<BR/>";
-        }
-        $updated_membership[] = array('rowpos' => $row, 'perid' => $cartrow['perid'], 'create_trans' => $master_perid, 'id' => $new_regid);
-        $cartrow['regid'] = $new_regid;
-        $cart_membership[$row]['regid'] = $new_regid;
-        $reg_ins++;
-    } else {
-        if (array_key_exists('todelete', $cartrow)) {
-            // delete membership
-            $paramarray = array($cartrow['regid']);
-            $typestr = 'i';
-            $reg_del += dbSafeCmd($delRegSQL, $typestr, $paramarray);
+
+    // remove l-r from phone
+    $cartrow['phone'] = trim(removeLROveride($cartrow['phone']));
+
+    if ($cartrow['perid'] <= 0) {
+        // insert this row
+        $paramarray = array(
+            $cartrow['last_name'],$cartrow['first_name'],$cartrow['middle_name'],$cartrow['suffix'],$cartrow['legalName'],$cartrow['pronouns'],
+            $cartrow['email_addr'],$cartrow['phone'],$cartrow['badge_name'],
+            $cartrow['address_1'],$cartrow['address_2'],$cartrow['city'],$cartrow['state'],$cartrow['postal_code'],$cartrow['country'],
+            $open_notes,$user_perid
+        );
+
+        $new_perid = dbSafeInsert($insPerinfoSQL, $insPDt, $paramarray);
+        if ($new_perid === false) {
+            $error_message .= "Insert of person $row failed<BR/>";
         } else {
-            // update membership
-            $paramarray = array($cartrow['price'], $cartrow['paid'],
-                                $cartrow['price'] > $cartrow['paid'] ? 'unpaid' : 'paid',
-                                $cartrow['memId'], $cartrow['regid']);
-            $typestr = 'ddsii';
-            $reg_upd += dbSafeCmd($updRegSQL, $typestr, $paramarray);
+            $cart_perinfo[$row]['perid'] = $new_perid;
+            $cartrow['perid'] = $new_perid;
+            $per_ins++;
         }
+    } else {
+        // update the row
+        $paramarray = array(
+            $cartrow['last_name'],$cartrow['first_name'],$cartrow['middle_name'],$cartrow['suffix'],$cartrow['legalName'],$cartrow['pronouns'],
+            $cartrow['email_addr'],$cartrow['phone'],$cartrow['badge_name'],
+            $cartrow['address_1'],$cartrow['address_2'],$cartrow['city'],$cartrow['state'],$cartrow['postal_code'],$cartrow['country'],$open_notes,
+            $user_perid, $cartrow['perid']
+        );
+        $per_upd += dbSafeCmd($updPerinfoSQL, $updPDt, $paramarray);
     }
-    if (!array_key_exists('todelete', $cartrow)) {
-        // now if there is a new note for this row, add it now
-        if (array_key_exists('new_reg_note', $cartrow)) {
-            $paramarray = array($user_perid, $master_transid, $cartrow['regid'], 'notes', $cartrow['new_reg_note']);
-            $typestr = 'iiiss';
-            $new_history = dbSafeInsert($insHistory, $typestr, $paramarray);
-            if ($new_history === false) {
-                $error_message .= 'Unable to add note to membership ' . $cartrow['regid'] . '<BR/>';
+
+    // Now process the memberships for this person
+    $memberships = $cartrow['memberships'];
+
+    for ($mrow = 0; $mrow < sizeof($memberships); $mrow++) {
+        $mbr = $memberships[$mrow];
+        if (!array_key_exists('coupon', $mbr))
+            $mbr['coupon'] = null;
+        if (!array_key_exists('couponDiscount', $mbr))
+            $mbr['couponDiscount'] = 0;
+
+        // if this row persists
+        if (!array_key_exists('todelete', $mbr)) {
+            if ($mbr['price'] == '')
+                $mbr['price'] = 0;
+            $total_price += $mbr['price'];
+
+            if ($mbr['paid'] == '')
+                $mbr['paid'] = 0;
+            $total_paid += $mbr['paid'];
+        }
+
+        if (!array_key_exists('regid', $mbr) || $mbr['regid'] <= 0) {
+            // insert the membership, as it's new
+            if ($mbr['perid'] <= 0) {
+                $mbr['perid'] = $cartrow['perid'];
+            }
+            $paramarray = array ($mbr['conid'], $mbr['perid'], $mbr['price'],
+                                 $mbr['price'] > $mbr['paid'] ? 'unpaid' : 'paid',
+                                 $mbr['paid'], $user_perid, $master_transid, $mbr['memId']);
+            $new_regid = dbSafeInsert($insRegSQL, $insRDt, $paramarray);
+            if ($new_regid === false) {
+                $error_message .= "Insert of membership $row failed<BR/>";
+            }
+            $mbr['regid'] = $new_regid;
+            $cart_perinfo[$row]['memberships'][$mrow]['regid'] = $new_regid;
+            $reg_ins++;
+        }
+        else {
+            if (array_key_exists('todelete', $mbr)) {
+                // delete membership
+                $paramarray = array ($mbr['regid']);
+                $reg_del += dbSafeCmd($delRegSQL, $delRDt, $paramarray);
+            }
+            else {
+                // update membership
+                $paramarray = array ($mbr['price'], $mbr['couponDiscount'], $mbr['paid'], $mbr['memId'], $mbr['coupon'],
+                                     $user_perid, $mbr['regid']);
+                $reg_upd += dbSafeCmd($updRegSQL, $updRDt, $paramarray);
+            }
+        }
+        if (!array_key_exists('todelete', $mbr)) {
+            // now if there is a new note for this row, add it now
+            if (array_key_exists('new_reg_note', $mbr)) {
+                $paramarray = array ($user_perid, $master_transid, $mbr['regid'], 'notes', $mbr['new_reg_note']);
+                $new_history = dbSafeInsert($insHistory, $insHDt, $paramarray);
+                if ($new_history === false) {
+                    $error_message .= 'Unable to add note to membership ' . $mbr['regid'] . '<BR/>';
+                }
             }
         }
     }
@@ -256,7 +226,7 @@ SET price = ?, paid = ?
 WHERE id = ?
 EOS;
 $paramarray = array($total_price, $total_paid, $master_transid);
-$typestr = 'ssi';
+$typestr = 'ddi';
 if (dbSafeCmd($updTransactionSQL, $typestr, $paramarray) === false) {
     $error_message .= "Update of master transaction failed";
 }
@@ -266,7 +236,6 @@ if ($error_message != '') {
     ajaxSuccess($response);
 }
 $response['message'] = "$per_ins members inserted, $per_upd members updated, $reg_ins memberships inserted, $reg_upd memberships updated, $reg_del memberships deleted";
-$response['updated_perinfo'] = $updated_perinfo;
-$response['updated_membership'] = $updated_membership;
+$response['updated_perinfo'] = $cart_perinfo;
 $response['master_tid'] = $master_transid;
 ajaxSuccess($response);
