b893cd3d63149a180e52dcf47db5919c1d0c4b36
Syd <63214729+sydwhv@users.noreply.github.com>
2024-10-22 10:51:45 -0400

    force new version

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -17,10 +17,10 @@ function getTabulatorIncludes(): array {
 
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
-$portalJSVersion = '0.3.24';
-$libJSversion = '0.3.23';
-$controllJSversion = '0.3.24';
-$globalJSversion = '0.3.23';
+$portalJSVersion = '0.3.28';
+$libJSversion = '0.3.28';
+$controllJSversion = '0.3.28';
+$globalJSversion = '0.3.28';
 $atJSversion = '0.3.9';
 $exhibitorJSversion = '0.3.10';
 $onlineregJSversion = '0.3.9';
