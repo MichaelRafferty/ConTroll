f3ab9da695d8699b0a579b9e65c294e849836e9f
Syd <63214729+sydwhv@users.noreply.github.com>
2024-10-29 13:17:46 -0400

    reorder main menu

---
+++ install/Database_Patch_Scripts/p35_authorder.sql
@@ -0,0 +1,14 @@
+/*
+ * update to allow sort order in auth entries for menu orderin
+ */
+
+ALTER TABLE auth  ADD COLUMN sortOrder int DEFAULT 0;
+UPDATE auth SET sortOrder = id * 100;
+
+UPDATE auth set sortOrder = 300 WHERE id = 6;
+UPDATE auth set sortOrder = 400 WHERE id = 3;
+UPDATE auth set sortOrder = 1050 WHERE id = 32;
+UPDATE auth set sortOrder = 550 WHERE id = 19;
+UPDATE auth set sortOrder = 520 WHERE id = 9;
+
+INSERT INTO patchLog(id, name) VALUES(35, 'auth order');
