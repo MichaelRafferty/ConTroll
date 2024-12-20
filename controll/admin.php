cb05e28e32e86bc7d5f69e58de7edd0ce193c89d
Syd <63214729+sydwhv@users.noreply.github.com>
2024-12-20 13:27:17 -0500

    Place holder for new menu items to come

--- controll/admin.php
+++ controll/admin.php
@@ -83,47 +83,6 @@ else
         </div>
     </div>
 </div>
-<div id='merge-lookup' class='modal modal-xl fade' tabindex='-1' aria-labelledby='Look up Merge Person' aria-hidden='true' style='--bs-modal-width: 80%;'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <div class='modal-header bg-primary text-bg-primary'>
-                <div class='modal-title'>
-                    <strong id='mergeTitle'>Lookup Person for Merge</strong>
-                </div>
-                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-            </div>
-            <div class='modal-body' style='padding: 4px; background-color: lightcyan;'>
-                <div class='container-fluid'>
-                    <form id='merge-search' action='javascript:void(0)'>
-                        <div class='row p-1'>
-                            <div class='col-sm-3 p-0'>
-                                <label for='merge_name_search' id="mergeName">Merge Name:</label>
-                            </div>
-                            <div class='col-sm-9 p-0'>
-                                <input class='form-control-sm' type='text' name='namesearch' id='merge_name_search' size='64'
-                                       placeholder='Name/Portion of Name, Person (Badge) ID'/>
-                            </div>
-                            <div class='row mt-3'>
-                                <div class='col-sm-12 text-bg-secondary'>
-                                    Search Results
-                                </div>
-                            </div>
-                            <div class='row'>
-                                <div class='col-sm-12' id='merge_search_results'>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class='modal-footer'>
-                <button class='btn btn-sm btn-secondary' data-bs-dismiss='modal'>Cancel</button>
-                <button class='btn btn-sm btn-primary' id='mergeSearch' onClick='merge_find()'>Find Person</button>
-            </div>
-            <div id='result_message_merge' class='mt-4 p-2'></div>
-        </div>
-    </div>
-</div>
 <div id='main'>
 
     <?php
@@ -151,7 +110,19 @@ else
     ?>
     <ul class="nav nav-tabs mb-3" id="admin-tab" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="users-tab" data-bs-toggle="pill" data-bs-target="#users-pane" type="button" role="tab" aria-controls="nav-users" aria-selected="true" onclick="settab('users-pane');">Users</button>
+            <button class="nav-link active" id="users-tab" data-bs-toggle="pill" data-bs-target="#users-pane" type="button" role="tab" aria-controls="nav-users"
+                    aria-selected="true" onclick="settab('users-pane');">Users
+            </button>
+        </li>
+        <li class='nav-item' role='presentation'>
+            <button class='nav-link' id='menu-tab' data-bs-toggle='pill' data-bs-target='#menu-pane' type='button' role='tab' aria-controls='nav-menu'
+                    aria-selected='false' onclick="settab('menu-pane');">Main Menu
+            </button>
+        </li>
+        <li class='nav-item' role='presentation'>
+            <button class='nav-link' id='keys-tab' data-bs-toggle='pill' data-bs-target='#keys-pane' type='button' role='tab' aria-controls='nav-menu'
+                    aria-selected='false' onclick="settab('keys-pane');">Client Keys
+            </button>
         </li>
     </ul>
     <div class="tab-content ms-2" id="admin-content">
@@ -258,7 +229,18 @@ else
             </table>
             <button id='add_new_account' type='button' class='btn btn-primary btn-sm' onclick="addFindPerson(); return false;">New Account</button>
         </div>
-    </div>
+        <div class='tab-pane fade' id='menu-pane' role='tabpanel' aria-labelledby='menu-tab' tabindex='0'>
+            <h1 class="h4">Drag and drop menu items to re-order main menu</h1>
+            <div class="row">
+                <div class="col-sm-12" id="menuTableDiv"></div>
+            </div>
+        </div>
+        <div class='tab-pane fade' id='keys-pane' role='tabpanel' aria-labelledby='keys-tab' tabindex='0'>
+            <h1 class='h4'>ConTroll Oauth2 Clients Configuration</h1>
+            <div class='row'>
+                <div class='col-sm-12' id='clientsTableDiv'></div>
+            </div>
+        </div>
     <div id='result_message' class='mt-4 p-2'></div>
 </div>
 <script>
