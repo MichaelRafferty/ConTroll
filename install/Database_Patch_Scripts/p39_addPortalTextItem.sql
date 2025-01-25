8d68e21f169c0bcfdbcb5ac7d04377a868d147dd
Syd <63214729+sydwhv@users.noreply.github.com>
2025-01-25 15:57:05 -0500

    more custom text catch-up stuff

--- install/Database_Patch_Scripts/p39_addPortalTextItem.sql
+++ install/Database_Patch_Scripts/p39_addPortalTextItem.sql
@@ -3,7 +3,6 @@
  */
 
 INSERT INTO `controllAppPages` VALUES
-    ('exhibitor','index','Exhibitor Portal Main Page - artist/vendor/fan/exhibits'),
     ('portal','accountSettings','Sets up management associations and identities for the Registation Portal'),
     ('portal','addUpgrade','Adds / Updates members including profile, interests and memberships for the Registration Portal'),
     ('portal','index','Login page for the Registration Portal'),
