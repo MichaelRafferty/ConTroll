e4dc266853ff1207a86052e2355e67f9c245a276
Syd <63214729+sydwhv@users.noreply.github.com>
2025-01-27 20:42:03 -0500

    assign an id to the lookup auth entry and not auto assign it

--- install/Database_Patch_Scripts/pxx_Post1.1.sql
+++ install/Database_Patch_Scripts/pxx_Post1.1.sql
@@ -2,7 +2,7 @@
  * Post the 1.1 release cleanup and requested immediate features patches
  */
 
-INSERT INTO auth(name, page, display, sortOrder)
-VALUES ('lookup', 'Y', 'Reg Lookup', 65);
+INSERT INTO auth(id, name, page, display, sortOrder)
+VALUES (8,'lookup', 'Y', 'Reg Lookup', 65);
 
 INSERT INTO patchLog(id, name) VALUES(xx, 'Post 1.1 Release Cleanup');
