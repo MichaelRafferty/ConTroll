5bc65ec9adcda89a51437d70434092c7c0e42c84
Syd <63214729+sydwhv@users.noreply.github.com>
2025-03-03 13:55:31 -0500

    bump for new gl fields

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -20,7 +20,7 @@ function getTabulatorIncludes(): array {
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '1.1.2';
 $libJSversion = '1.1.9';
-$controllJSversion = '1.1.10';
+$controllJSversion = '1.1.11';
 $globalJSversion = '1.1.6';
 $atJSversion = '1.1.4';
 $exhibitorJSversion = '1.1.4';
