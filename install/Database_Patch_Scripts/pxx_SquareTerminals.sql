72ac61b37184304ce9753e618095e55f46bfcce4
Syd <63214729+sydwhv@users.noreply.github.com>
2025-04-01 12:49:34 -0400

    need to track what fee paid and in what transaction for receipts

--- install/Database_Patch_Scripts/pxx_SquareTerminals.sql
+++ install/Database_Patch_Scripts/pxx_SquareTerminals.sql
@@ -25,12 +25,19 @@ CREATE TABLE terminals (
     PRIMARY KEY (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+/* change to deal with taxable items in the 'membership category */
 ALTER TABLE memCategories ADD COLUMN taxable enum('Y','N') NOT NULL DEFAULT 'N' AFTER variablePrice;
 
 INSERT INTO memCategories(memCategory, notes, onlyOne, standAlone, variablePrice, taxable, badgeLabel, active, sortorder)
 VALUES ('addonTaxable', 'Req: Taxable add-on'' to memberships', 'N', 'Y', 'N', 'Y', 'X', 'Y', 75 );
 
+/* changes for splitting orders from payments for square, leading to terminal usage */
 ALTER TABLE transaction ADD COLUMN orderId varchar(64) DEFAULT NULL AFTER coupon;
 ALTER TABLE transaction ADD COLUMN orderDate datetime DEFAULT NULL AFTER orderId;
+
+/* changes for mail in fee receipt ability */
+ALTER TABLE exhibitorYears ADD COLUMN mailinFeePaidAmount decimal(8, 2) DEFAULT NULL AFTER mailin;
+ALTER TABLE exhibitorYears ADD COLUMN mailinFeeTransaction int DEFAULT NULL AFTER mailinFeePaidAmount;
+ALTER TABLE exhibitorYears ADD FOREIGN KEY ey_mailintrans(mailinFeeTransaction) REFERENCES transaction(id) ON UPDATE CASCADE;
 
 INSERT INTO patchLog(id, name) VALUES(xx, 'Square Terminals');
