94c4e5e0bd2d3368a1c5306a5d43f11e09d37492
Syd <63214729+sydwhv@users.noreply.github.com>
2025-04-07 22:02:07 -0400

    first try at status update

--- atcon/scripts/admin_getTerminalStatus.php
+++ atcon/scripts/admin_getTerminalStatus.php
@@ -36,10 +36,122 @@ if (!array_key_exists('terminal', $_POST)) {
 
 $terminal = $_POST['terminal'];
 load_term_procs();
-$status = term_getStatus($terminal);
-
-
 // get the status from the terminal
+$apiResult = term_getStatus($terminal);
+
+// update the database
+$upSQL = <<<EOS
+UPDATE terminals
+SET
+    productType = ?,
+    squareName = ?,
+    squareModel = ?,
+    version = ?,
+    terminalAPIVersion = ?,
+    batteryLevel = ?,
+    externalPower = ?,
+    wifiActive = ?,
+    wifiSSID = ?,
+    wifiIPAddressV4 = ?,
+    wifiIPAddressV6 = ?,
+    signalStrength = ?,
+    ethernetActive = ?,
+    ethernetIPAddressV4 = ?,
+    ethernetIPAddressV6 = ?,
+    status = ?,
+    statusChanged = now()
+WHERE name = ?;
+EOS;
+    $attributes = $apiResult['attributes'];
+    $components = $apiResult['components'];
+    $application = null;
+    $battery = null;
+    $wifi = null;
+    $ethernet = null;
+    foreach ($components as $component) {
+        switch ($component['type']) {
+            case 'APPLICATION':
+                $application = $component;
+                break;
+            case 'BATTERY':
+                $battery = $component;
+                break;
+            case 'WIFI':
+                $wifi = $component;
+                break;
+            case 'ETHERNET':
+                $ethernet = $component;
+                break;
+        }
+    }
+    $status = $apiResult['status'];
+
+    // now the fields
+
+    $version = $attributes['version'];
+    if ($application) {
+        $productType = $application['application_details']['application_type'];
+        $terminalAPIVersion = $application['application_details']['version'];
+    } else {
+        $productType = null;
+        $terminalAPIVersion = null;
+    }
+
+    $squareName = $attributes['name'];
+    $squareModel = $attributes['model'];
+
+    if ($battery) {
+        $batteryLevel = $battery['battery_details']['visible_percent'];
+        $externalPower = $battery['battery_details']['external_power'];
+    } else {
+        $batteryLevel = null;
+        $externalPower = null;
+    }
+
+    if ($wifi) {
+        $wifiActive = $wifi['wifi_details']['active'] ? true : false;
+        $wifiSSID = $wifi['wifi_details']['ssid'];
+        $signalStrength = $wifi['wifi_details']['signal_strength']['value'];
+        if (array_key_exists('ip_address_v4', $wifi['wifi_details']))
+            $wifiIPAddressV4 = $wifi['wifi_details']['ip_address_v4'];
+        else
+            $wifiIPAddressV4 = null;
+        if (array_key_exists('ip_address_v6', $wifi['wifi_details']))
+            $wifiIPAddressV6 = $wifi['wifi_details']['ip_address_v6'];
+        else
+            $wifiIPAddressV6 = null;
+    } else {
+        $wifiActive = null;
+        $wifiSSID = null;
+        $signalStrength = null;
+        $wifiIPAddressV4 = null;
+        $wifiIPAddressV6 = null;
+    }
+    if ($ethernet) {
+        $ethernet = $ethernet['ethernet_details']['active'] ? true : false;
+        if (array_key_exists('ip_address_v4', $ethernet['ethernet_details']))
+            $ethernetIPAddressV4 = $ethernet['ethernet_details']['ip_address_v4'];
+        else
+            $ethernetIPAddressV4 = null;
+        if (array_key_exists('ip_address_v6', $ethernet['ethernet_details']))
+            $ethernetIPAddressV6 = $ethernet['ethernet_details']['ip_address_v6'];
+        else
+            $ethernetIPAddressV6 = null;
+    } else {
+        $ethernetActive = null;
+        $ethernetIPAddressV4 = null;
+        $ethernetIPAddressV6 = null;
+    }
+
+    $statusCat = $status['category'];
+
+    $arrVals = array($productType, $squareName, $squareModel, $version, $terminalAPIVersion, $batteryLevel, $externalPower,
+        $wifiActive, $wifiSSID, $wifiIPAddressV4, $wifiIPAddressV6, $signalStrength,
+        $ethernetActive, $ethernetIPAddressV4, $ethernetIPAddressV6, $statusCat);
+
+    $datatypes = 'sssssisisssiisss';
+    $updCnt = dbSafeCmd($upSQL, $datatypes, $arrVals);
+
 
 // fetch the updated terminal record
 $terminalSQL = <<<EOS
@@ -56,5 +168,5 @@ $updatedRow = $terminalQ->fetch_assoc();
 $response['updatedRow'] = $updatedRow;
 mysqli_free_result($terminalQ);
 
-$response['message'] = "$terminal status updated";
+$response['message'] = "$terminal status updated, $updCnt row updated";
 ajaxSuccess($response);
