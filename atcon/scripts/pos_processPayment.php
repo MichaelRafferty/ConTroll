7d7dbfee2a4f6e0a33e1880db6aa91ce8ef10ae1
Syd <63214729+sydwhv@users.noreply.github.com>
2025-06-03 12:00:09 -0400

    latest

--- atcon/scripts/pos_processPayment.php
+++ atcon/scripts/pos_processPayment.php
@@ -99,7 +99,7 @@ if (array_key_exists('discountAmt', $_POST))
 else
     $discountAmt = 0;
 
-$preTaxAmt -= (int)$couponDiscount + (int)$discountAmt;
+$preTaxAmt -= $couponDiscount + $discountAmt;
 
 if ($amt != $preTaxAmt + $taxAmt) {
     ajaxError('Invalid payment amount passed: preTax + Tax != Amount');
