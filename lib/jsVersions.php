59fa1731a55f6058ea8c349b7825332ce0b561dc
Syd <63214729+sydwhv@users.noreply.github.com>
2025-06-10 19:22:22 -0400

    for changes

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -19,9 +19,9 @@ function getTabulatorIncludes(): array {
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '1.3.0';
-$libJSversion = '1.3.6';
-$controllJSversion = '1.3.5';
-$globalJSversion = '1.3.2';
-$atJSversion = '1.3.1';
-$exhibitorJSversion = '1.3.2';
+$libJSversion = '1.3.7';
+$controllJSversion = '1.3.6';
+$globalJSversion = '1.3.3';
+$atJSversion = '1.3.2';
+$exhibitorJSversion = '1.3.3';
 $onlineregJSversion = '1.3.0';
