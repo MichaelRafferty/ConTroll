ef1b4e445e56e528ea94d704f66989141420f9ba
Syd <63214729+sydwhv@users.noreply.github.com>
2025-06-16 12:16:16 -0400

    add performance and site selection tokens

--- install/Database_Patch_Scripts/p48_artshow.php
+++ install/Database_Patch_Scripts/p48_artshow.php
@@ -1,5 +1,7 @@
 /* P48
  * add limits and notes to exhibitors
+ * improve performace
+ * add site selection items for worldcons
  */
 
 /*
@@ -14,4 +16,24 @@ ALTER TABLE exhibitsRegionYears ADD mailinGLNum varchar(16) CHARACTER SET utf8mb
 ALTER TABLE exhibitsRegionYears ADD mailinGLLabel varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL AFTER mailinGLNum;
 ALTER TABLE exhibitsRegionYears ADD roomStatus enum('precon', 'bid', 'checkout', 'closed', 'all') DEFAULT 'precon' AFTER exhibitsRegion;
 
-INSERT INTO patchLog(id, name) VALUES(xx, 'artshow');
+/*
+ * some performance improvement indicies
+ */
+CREATE INDEX perinfo_idx_email ON perinfo(email_addr);
+CREATE INDEX perinfoIdent_idx_email ON perinfoIdentities(email_addr);
+CREATE INDEX exhibitors_idx_email ON exhibitors(exhibitorEmail);
+CREATE INDEX exhibitorYears_idx_email ON exhibitorYears(contactEmail);
+
+/*
+ * site selection table
+ */
+CREATE TABLE siteSelectionTokens (
+    id int NOT NULL AUTO_INCREMENT,
+    tokenKey varchar(64) NOT NULL,
+    perid int DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE siteSelectionTokens ADD CONSTRAINT `sst_perinfo_fk` FOREIGN KEY (perid) REFERENCES perinfo(id) ON UPDATE CASCADE;
+
+
+INSERT INTO patchLog(id, name) VALUES(xx, 'artshow-siteselection');
