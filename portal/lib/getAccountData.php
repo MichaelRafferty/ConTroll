f175f7654659105f1f9c32803609ed2b018daf38
Syd <63214729+sydwhv@users.noreply.github.com>
2025-07-18 13:22:46 -0400

    found one more place without fname

--- portal/lib/getAccountData.php
+++ portal/lib/getAccountData.php
@@ -26,6 +26,11 @@ WITH trans AS (
         IFNULL(tp.complete_date, t.create_date) AS transDate,
         m.label, m.memAge, m.memAge AS age, m.memType, m.memCategory, m.startdate, m.enddate, m.online, 
         m.taxable, m.ageShortName AS ageshortname, m.shortname,
+         CASE 
+            WHEN pn.id IS NOT NULL THEN pn.first_name
+            WHEN nn.id IS NOT NULL THEN nn.first_name
+            ELSE NULL
+        END AS fname,
         CASE 
             WHEN pn.id IS NOT NULL THEN pn.managedBy
             WHEN nn.id IS NOT NULL THEN nn.managedBy
@@ -78,7 +83,7 @@ WITH trans AS (
         CASE WHEN r.complete_trans IS NULL THEN r.create_trans ELSE r.complete_trans END AS sortTrans,
         CASE WHEN tp.complete_date IS NULL THEN t.create_date ELSE tp.complete_date END AS transDate,
         m.label, m.memAge, m.memAge AS age, m.memType, m.memCategory,  m.startdate, m.enddate, m.online, m.taxable, 
-        m.ageShortName, m.shortname, nn.managedBy, nn.managedByNew, nn.badge_name, 
+        m.ageShortName, m.shortname, nn.first_name AS fname, nn.managedBy, nn.managedByNew, nn.badge_name, 
         TRIM(REGEXP_REPLACE(CONCAT(nn.first_name, ' ', nn.middle_name, ' ', nn.last_name, ' ', nn.suffix), '  *', ' ')) AS fullName, 
         nn.id as memberId, nn.email_addr, nn.phone,
         IFNULL(tp.perid, t.perid) AS transPerid,
@@ -102,7 +107,7 @@ WITH mems AS (
     SELECT t.id, r.create_date, r.id AS regid, r.memId, r.conid, r.status, r.price, r.paid, r.complete_trans,
         r.couponDiscount, r.perid, r.newperid,
         m.label, m.memAge, m.memAge AS age, m.memType, m.memCategory,  m.startdate, m.enddate, m.online, m.taxable, 
-        m.ageShortName, m.shortname, p.managedBy, p.managedByNew,
+        m.ageShortName, m.shortname, p.first_name AS fname, p.managedBy, p.managedByNew,
         CASE WHEN r.complete_trans IS NULL THEN r.create_trans ELSE r.complete_trans END AS sortTrans,
         CASE WHEN tp.complete_date IS NULL THEN t.create_date ELSE tp.complete_date END AS transDate,
         CASE 
