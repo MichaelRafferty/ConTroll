18cb51921d6b7bb59486ea1cbcf3b56c398cce62
Syd <63214729+sydwhv@users.noreply.github.com>
2025-07-25 11:51:06 -0400

    change location(s) from null to empty string

--- install/Database_Patch_Scripts/p51_artshowItems.sql
+++ install/Database_Patch_Scripts/p51_artshowItems.sql
@@ -6,5 +6,11 @@
 DELETE FROM atcon_auth WHERE auth = 'artshow';
 ALTER TABLE atcon_auth MODIFY COLUMN  auth enum('data_entry','cashier','manager','artinventory','artsales','vol_roll');
 
+UPDATE exhibitorRegionYears SET locations = '' WHERE locations IS NULL;
+ALTER TABLE exhibitorRegionYears MODIFY COLUMN locations  varchar(512) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '';
+
+UPDATE artItems SET location = '' WHERE location IS NULL;
+ALTER TABLE artItems MODIFY COLUMN location varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '';
+
 
 INSERT INTO patchLog(id, name) VALUES(xx, 'artshowItems');
