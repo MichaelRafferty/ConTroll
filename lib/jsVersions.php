e24bddb6b70c7ea417f2e5ad52d400af15335c0f
Syd <63214729+sydwhv@users.noreply.github.com>
2025-07-29 19:44:21 -0400

    update optional luxon package to latest

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -6,7 +6,7 @@ function getTabulatorIncludes(): array {
         'tabcss' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator.min.css',
         'tabbs5' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator_bootstrap5.min.css',
         'tabjs' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/js/tabulator.min.js',
-        'luxon' => 'https://cdn.jsdelivr.net/npm/luxon@3.5.0/build/global/luxon.min.js',
+        'luxon' => 'https://cdn.jsdelivr.net/npm/luxon@3.7.1/build/global/luxon.min.js',
         'bs5css' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr' crossorigin='anonymous'",
         'bs5js' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js' integrity='sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q' crossorigin='anonymous'",
         'popjs' => 'https://unpkg.com/@popperjs/core@2',
