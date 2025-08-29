212e2a689266f264d8d94cf8318880926e5ca342
Syd <63214729+sydwhv@users.noreply.github.com>
2025-08-29 14:12:29 -0400

    package tFPDF (fpdf) and barcode variant of it as part of the distro due to font issues and needing change to tFPDF from Fpdf for utf-8/unicode fonts

---
+++ lib/pdf/fpdf-barcode/src/Barcode.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once ("type/Code128.php");
+require_once ("type/Code39.php");
+require_once ("type/Ean13.php");
+require_once ("type/UpcA.php");
+
+class Barcode extends tFPDF
+{
+    /** @var array */
+    protected $barcodes;
+
+    public function __construct($orientation='P', $unit='mm', $size='A4')
+    {
+        $this->barcodes = array();
+        parent::__construct($orientation, $unit, $size);
+    }
+
+    /**
+     * @param float $x
+     * @param float $y
+     * @param string $code
+     * @param float $width
+     * @param float $height
+     */
+    public function code128($x, $y, $code, $width, $height)
+    {
+        if (!isset($this->barcodes['code128'])) {
+            $this->barcodes['code128'] = new Code128($this);
+        }
+
+        $this->barcodes['code128']->drawCode($x, $y, $code, $width, $height);
+    }
+
+    /**
+     * @param float $x
+     * @param float $y
+     * @param string $code
+     * @param float $width
+     * @param float $height
+     * @param bool $isWide
+     * @param bool $extended
+     * @param bool $needChecksum
+     * @param bool $displayText
+     */
+    public function code39($x, $y, $code, $width=0.4, $height = 20.0, $isWide = false, $extended = true, $needChecksum = false, $displayText = false)
+    {
+        if (!isset($this->barcodes['code39'])) {
+            $this->barcodes['code39'] = new Code39($this);
+        }
+
+        $this->barcodes['code39']->drawCode($x, $y, $code, $extended, $needChecksum, $width, $height, $isWide, $displayText);
+    }
+
+    /**
+     * @param float $x
+     * @param float $y
+     * @param string $code
+     * @param float $width
+     * @param float $height
+     */
+    public function ean13($x, $y, $code, $width = .35, $height = 16.0)
+    {
+        if (!isset($this->barcodes['ean13'])) {
+            $this->barcodes['ean13'] = new Ean13($this);
+        }
+
+        $this->barcodes['ean13']->drawCode($x, $y, $code, $height, $width);
+    }
+
+    /**
+     * @param float $x
+     * @param float $y
+     * @param string $code
+     * @param float $width
+     * @param float $height
+     */
+    public function upcA($x, $y, $code, $width = .35, $height = 16.0)
+    {
+        if (!isset($this->barcodes['upca'])) {
+            $this->barcodes['upca'] = new UpcA($this);
+        }
+
+        $this->barcodes['upca']->drawCode($x, $y, $code, $height, $width);
+    }
+}
