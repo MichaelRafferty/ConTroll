ed4da03c18e8b953ac86fdb1f30fa754c2666972
Syd <63214729+sydwhv@users.noreply.github.com>
2026-01-14 13:01:52 -0500

    add null checkrefresh due to jslib items

--- portal/js/base.js
+++ portal/js/base.js
@@ -24,3 +24,8 @@ function checkResolveUpdates(data) {
         }
     }
 }
+
+// new functions for token items
+function checkRefresh(data) {
+    // empty function for common js files for controll and atcon
+}
