24d1dcf9d3dc7fee0b6dec3b61d95aebde4944aa
Syd <63214729+sydwhv@users.noreply.github.com>
2026-02-06 15:59:39 -0500

    accept conid on the command line and adapt for control sheets only

--- vendor/scripts/bidsheets.php
+++ vendor/scripts/bidsheets.php
@@ -25,9 +25,43 @@ if(!array_key_exists('type', $_GET) || !array_key_exists('region', $_GET) || !is
 }
 
 $eyID = getSessionVar('eyID');
-$region = $_GET['region'];
+$conid = getConfValue('con', 'id');
+if (array_key_exists('region', $_REQUEST))
+    $region = $_REQUEST['region'];
+else {
+    $response['error'] = 'Invalid calling sequence';
+    echo "<h1>Invalid calling sequence</h1>\n";
+    return $response;
+}
 
-switch($_GET['type']) {
+if (array_key_exists('type', $_REQUEST)) {
+    $type = $_REQUEST['type'];
+} else {
+    $type = 'unknown';
+}
+
+if ($type == 'control' &&  array_key_exists('conid', $_REQUEST)) {
+    $conyear = $_REQUEST['conid'];
+    if ($conid != $conyear && $conyear > 0) {
+        // translate region to the appropriate region for that year
+        $cyQ = <<<EOS
+SELECT erycy.id, exycy.id
+FROM exhibitsRegionYears ery 
+JOIN exhibitsRegionYears erycy ON ery.exhibitsRegion = erycy.exhibitsRegion
+join exhibitorYears exy ON exy.id = ?
+join exhibitorYears exycy ON exy.exhibitorId = exycy.exhibitorId and exycy.conid = erycy.conid
+where ery.id = ? and erycy.conid = ?;
+EOS;
+        $cyR = dbSafeQuery($cyQ, 'iii', array($eyID, $region, $conyear));
+        if ($cyR !== false && $cyR->num_rows == 1) {
+            [$region, $eyID] = $cyR->fetch_row();
+            $cyR->free();
+        }
+    }
+}
+
+
+switch($_REQUEST['type']) {
     case 'bidsheets':
         $response = pdfPrintBidSheets($eyID, $region, $response);
         break;
@@ -38,4 +72,6 @@ switch($_GET['type']) {
         $response = pdfArtistControlSheet($eyID, $region, $response);
         break;
     default:
+        echo "<h1>Error Invalid sheet type, please seek assistance</h1\n";
+
 }
