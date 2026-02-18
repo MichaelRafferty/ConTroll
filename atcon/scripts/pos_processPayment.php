19915f5706c98f5392cf43d7c15d696140aff253
Syd <63214729+sydwhv@users.noreply.github.com>
2026-02-18 11:53:32 -0500

    protect from card details being null

--- atcon/scripts/pos_processPayment.php
+++ atcon/scripts/pos_processPayment.php
@@ -442,8 +442,18 @@ EOS;
             $last4 = $payment['card_details']['card']['last_4'];
             $id = $payment['id'];
             $location_id = $payment['location_id'];
-            $auth = $payment['card_details']['auth_result_code'];
-            $nonce = $payment['card_details']['card']['fingerprint'];
+            if (array_key_exists(''))
+            if (array_key_exists('card_details')) {
+                if (array_key_exists('auth_result_code', $payment['card_details']))
+                    $auth = $payment['card_details']['auth_result_code'];
+                else
+                    $auth = null;
+
+                if (array_key_exists('fingerprint', $payment['card_details']))
+                    $nonce = $payment['card_details']['card']['fingerprint'];
+                else
+                    $nonce = null;
+            }
             $status = $payment['status'];
             switch ($payment['source_type']) {
                 case 'CARD':
