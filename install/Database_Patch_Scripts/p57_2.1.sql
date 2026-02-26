d9fa016ad00e4767a2a4d13a2da7a6e8fdf71bb0
Syd <63214729+sydwhv@users.noreply.github.com>
2026-02-26 15:00:30 -0500

    start of new cart

---
+++ install/Database_Patch_Scripts/p57_2.1.sql
@@ -0,0 +1,19 @@
+/*
+ * P57 - continued refinement of Registration Portal and extensions of cross year back end
+ */
+
+ALTER TABLE memList ADD COLUMN cartDesc text DEFAULT null AFTER label;
+
+DROP VIEW IF EXISTS `memLabel`;
+CREATE ALGORITHM=UNDEFINED
+    SQL SECURITY INVOKER
+    VIEW memLabel AS
+SELECT m.id AS id,m.conid AS conid,m.sort_order AS sort_order,m.memCategory AS memCategory,m.memType AS memType,m.memAge AS memAge,
+       a.shortname AS ageShortName,m.label AS shortname,concat(m.label,' [',a.label,']') AS label,m.cartDesc AS cartDesc,
+       m.notes AS notes,m.price AS price, m.startdate AS startdate,m.enddate AS enddate,
+       m.atcon AS atcon,m.online AS online,m.glNum AS glNum,m.glLabel AS glLabel, c.taxable AS taxable
+FROM memList m
+JOIN ageList a ON (m.memAge = a.ageType) AND (m.conid = a.conid)
+JOIN memCategories c ON m.memCategory = c.memCategory;
+
+INSERT INTO patchLog(id, name) VALUES(x57, 'Release 2.1 Portal and other changes');
