859adfdd3a8a6b22ec72f9b6ad311ee9b7253631
Syd <63214729+sydwhv@users.noreply.github.com>
2026-03-08 14:30:17 -0400

    bump version

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -3,9 +3,9 @@
 // getTabulatorIncludes - returns CDN string for Tabulator
 function getTabulatorIncludes(): array {
     return ( [
-        'tabcss' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator.min.css',
-        'tabbs5' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator_bootstrap5.min.css',
-        'tabjs' => 'https://unpkg.com/tabulator-tables@6.3.1/dist/js/tabulator.min.js',
+        'tabcss' => 'https://unpkg.com/tabulator-tables@6.4.0/dist/css/tabulator.min.css',
+        'tabbs5' => 'https://unpkg.com/tabulator-tables@6.4.0/dist/css/tabulator_bootstrap5.min.css',
+        'tabjs' => 'https://unpkg.com/tabulator-tables@6.4.0/dist/js/tabulator.min.js',
         'luxon' => 'https://cdn.jsdelivr.net/npm/luxon@3.7.1/build/global/luxon.min.js',
         'bs5css' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet' integrity = 'sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB' crossorigin='anonymous'",
         'bs5js' => "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js' integrity='sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI' crossorigin='anonymous'",
@@ -13,6 +13,9 @@ function getTabulatorIncludes(): array {
         'jqjs' => '/jslib/jquery-3.7.1.min.js',
         'jquijs' => '/jslib/jquery-ui.min-1.13.1.js',
         'jquicss' => '/csslib/jquery-ui-1.13.1.css',
+
+
+
     ]);
 }
 
@@ -20,7 +23,7 @@ function getTabulatorIncludes(): array {
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.0.6';
 $libJSversion = '2.0.4';
-$controllJSversion = '2.0.5';
+$controllJSversion = '2.0.6';
 $globalJSversion = '2.0.3';
 $atJSversion = '2.0.0';
 $exhibitorJSversion = '2.0.0';
