854cd5577ce79b0c4f4e9132e280e8f35a226493
Syd <63214729+sydwhv@users.noreply.github.com>
2026-03-31 09:06:41 -0400

    add scroll message into view

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -22,9 +22,9 @@ function getTabulatorIncludes(): array {
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.0.9';
-$libJSversion = '2.0.12';
+$libJSversion = '2.0.13';
 $controllJSversion = '2.0.25';
-$globalJSversion = '2.0.4';
+$globalJSversion = '2.0.5';
 $atJSversion = '2.0.4';
 $exhibitorJSversion = '2.0.7';
 $onlineregJSversion = '2.0.2';
