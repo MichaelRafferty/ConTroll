a786acf379cbed7add4f8fc15e3e4ddf5019db92
Syd <63214729+sydwhv@users.noreply.github.com>
2026-04-19 17:12:19 -0400

    bump

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -13,9 +13,6 @@ function getTabulatorIncludes(): array {
         'jqjs' => '/jslib/jquery-3.7.1.min.js',
         'jquijs' => '/jslib/jquery-ui.min-1.13.1.js',
         'jquicss' => '/csslib/jquery-ui-1.13.1.css',
-
-
-
     ]);
 }
 
@@ -23,7 +20,7 @@ function getTabulatorIncludes(): array {
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.0.19';
 $libJSversion = '2.0.22';
-$controllJSversion = '2.0.34';
+$controllJSversion = '2.0.35';
 $globalJSversion = '2.0.9';
 $atJSversion = '2.0.7';
 $exhibitorJSversion = '2.0.11';
