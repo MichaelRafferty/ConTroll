9290703f8967baa8721a81f47f798936077a8809
Syd <63214729+sydwhv@users.noreply.github.com>
2026-07-06 13:35:22 -0400

    restrict shortname length when adding Bundle: to the front of it.

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -20,7 +20,7 @@ function getTabulatorIncludes(): array {
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.2.0';
 $libJSversion = '2.2.0';
-$controllJSversion = '2.2.1';
+$controllJSversion = '2.2.2';
 $globalJSversion = '2.2.0';
 $atJSversion = '2.2.0';
 $exhibitorJSversion = '2.2.0';
