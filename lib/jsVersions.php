713e3440c2510c739255b266bfaf399d3c4fdc4e
Syd <63214729+sydwhv@users.noreply.github.com>
2026-07-25 15:48:07 -0400

    bump

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -19,7 +19,7 @@ function getTabulatorIncludes(): array {
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.2.1';
-$libJSversion = '2.2.1';
+$libJSversion = '2.2.2';
 $controllJSversion = '2.2.1';
 $globalJSversion = '2.2.0';
 $atJSversion = '2.2.0';
