c3d72d483f3fc38518daaf38cbb2f568000e295c
Syd <63214729+sydwhv@users.noreply.github.com>
2026-08-09 16:37:50 -0400

    add empty message check

--- controll/scripts/sendEmail.php
+++ controll/scripts/sendEmail.php
@@ -338,7 +338,15 @@ default:
     exit();
 }
 
-if (str_contains($email_text, 'Controll-Default: This is ') || str_contains($email_html, 'Controll-Default: This is ')) {
+if ($email_text == '' || $email_html == '') {
+    $response['error'] = "The custom text of the text or html version of the $email_type has not been entered yet.<br/>" .
+        'It needs to be edited before sending emails of this type.';
+    ajaxSuccess($response);
+    exit();
+}
+
+if (str_contains($email_text, 'Controll-Default: This is ') ||
+    str_contains($email_html, 'Controll-Default: This is ')) {
     $response['error'] = "The custom text of the text or html version of the $email_type is still the default message.<br/>" .
         "It needs to be edited before sending emails of this type.";
     ajaxSuccess($response);
