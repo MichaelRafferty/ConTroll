afa6fb4f7a42dd5f0c52ab9ad83deaee48212a1f
Syd <63214729+sydwhv@users.noreply.github.com>
2026-08-21 19:35:04 -0400

    fix accidental delete of function call name

--- controll/js/login.js
+++ controll/js/login.js
@@ -24,7 +24,7 @@ class Login {
         if (this.#loginWithPasskeyBtn)
             this.#loginWithPasskeyBtn.disabled = true;
 
-        ('scripts/passkeyActions.php', 'index.php', 'controll', this.#loginWithPasskeyBtn, email);
+        passkeyRequest('scripts/passkeyActions.php', 'index.php', 'controll', this.#loginWithPasskeyBtn, email);
     }
 
     // loginWithToken: show email for token
