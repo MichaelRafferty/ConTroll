3dc476c58f8888722f5cb302b216a6c3d75ba0bb
Syd <63214729+sydwhv@users.noreply.github.com>
2026-08-30 21:23:58 -0400

    bump for full 2.3 release

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -18,10 +18,10 @@ function getTabulatorIncludes(): array {
 
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
-$portalJSVersion = '2.2.13';
-$libJSversion = '2.2.26';
-$controllJSversion = '2.2.25';
-$globalJSversion = '2.2.7';
-$atJSversion = '2.2.13';
-$exhibitorJSversion = '2.2.11';
-$onlineregJSversion = '2.2.4';
+$portalJSVersion = '2.3.0';
+$libJSversion = '2.3.0';
+$controllJSversion = '2.3.0';
+$globalJSversion = '2.3.0';
+$atJSversion = '2.3.0';
+$exhibitorJSversion = '2.3.0';
+$onlineregJSversion = '2.3.0';
