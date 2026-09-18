7bdbe3a9570baa87d7d7e7dde8766e5db08f5074
Syd <63214729+sydwhv@users.noreply.github.com>
2026-09-18 15:43:47 -0400

    fix issue with filters staying on as you switch tabs

--- lib/jsVersions.php
+++ lib/jsVersions.php
@@ -19,8 +19,8 @@ function getTabulatorIncludes(): array {
 // JS Version items
 global $portalJSVersion, $libJSversion, $controllJSversion, $globalJSversion, $atJSversion, $exhibitorJSversion, $onlineregJSversion;
 $portalJSVersion = '2.3.0';
-$libJSversion = '2.3.1';
-$controllJSversion = '2.3.4';
+$libJSversion = '2.3.4';
+$controllJSversion = '2.3.6';
 $globalJSversion = '2.3.0';
 $atJSversion = '2.3.0';
 $exhibitorJSversion = '2.3.0';
